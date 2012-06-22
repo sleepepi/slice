1ed2a131b15e601115863b29d3eb0e0426f671a0
Remo Mueller <remosm@gmail.com>
2012-06-22 17:03:34 -0400

    Version 0.3.1 finalized

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -3,7 +3,7 @@ module Reading
     MAJOR = 0
     MINOR = 3
     TINY = 1
-    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
