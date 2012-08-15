0d859470569fe8dc730001ca6f7271a087d1ad1b
Remo Mueller <remosm@gmail.com>
2012-08-15 09:23:03 -0400

    Version bump to 0.7.0.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -1,9 +1,9 @@
 module Reading
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 6
+    MINOR = 7
     TINY = 0
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
