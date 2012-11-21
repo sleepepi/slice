33b74451e03bf1b72b2bc9cc74879ac865304bf1
Remo Mueller <remosm@gmail.com>
2012-11-21 11:51:27 -0500

    Added subject functional tests

--- test/functional/subjects_controller_test.rb
+++ test/functional/subjects_controller_test.rb
@@ -13,6 +13,12 @@ class SubjectsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:subjects)
   end
 
+  test "should not get index with invalid project" do
+    get :index, project_id: -1
+    assert_nil assigns(:subjects)
+    assert_redirected_to root_path
+  end
+
   test "should get paginated index" do
     get :index, project_id: @project, format: 'js'
     assert_not_nil assigns(:subjects)
@@ -24,12 +30,31 @@ class SubjectsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get new subject with invalid project" do
+    get :new, project_id: -1
+
+    assert_not_nil assigns(:subject)
+    assert_nil assigns(:project)
+    assert_redirected_to root_path
+  end
+
   test "should create subject" do
     assert_difference('Subject.count') do
       post :create, project_id: @project, subject: { subject_code: 'Code03', status: @subject.status }, site_id: @subject.site_id
     end
 
     assert_redirected_to project_subject_path(assigns(:subject).project, assigns(:subject))
+  end
+
+  test "should not create subject with blank subject code" do
+    assert_difference('Site.count', 0) do
+      post :create, project_id: @project, subject: { subject_code: '', status: @subject.status }, site_id: @subject.site_id
+    end
+
+    assert_not_nil assigns(:subject)
+    assert assigns(:subject).errors.size > 0
+    assert_equal ["can't be blank"], assigns(:subject).errors[:subject_code]
+    assert_template 'new'
   end
 
   test "should not create subject for invalid project" do
@@ -55,6 +80,7 @@ class SubjectsControllerTest < ActionController::TestCase
 
   test "should show subject" do
     get :show, id: @subject, project_id: @project
+    assert_not_nil assigns(:project)
     assert_not_nil assigns(:subject)
     assert_response :success
   end
@@ -62,14 +88,23 @@ class SubjectsControllerTest < ActionController::TestCase
   test "should show subject to site user" do
     login(users(:site_one_user))
     get :show, id: @subject, project_id: @project
+    assert_not_nil assigns(:project)
     assert_not_nil assigns(:subject)
     assert_response :success
   end
 
   test "should not show invalid subject" do
     get :show, id: -1, project_id: @project
+    assert_not_nil assigns(:project)
     assert_nil assigns(:subject)
     assert_redirected_to project_subjects_path
+  end
+
+  test "should not show subject with invalid project" do
+    get :show, id: @subject, project_id: -1
+    assert_not_nil assigns(:subject)
+    assert_nil assigns(:project)
+    assert_redirected_to root_path
   end
 
   test "should not show subject on different site to site user" do
@@ -82,6 +117,24 @@ class SubjectsControllerTest < ActionController::TestCase
   test "should get edit" do
     get :edit, id: @subject, project_id: @project
     assert_response :success
+  end
+
+  test "should not get edit for invalid subject" do
+    get :edit, id: -1, project_id: @project
+
+    assert_not_nil assigns(:project)
+    assert_nil assigns(:subject)
+
+    assert_redirected_to project_subjects_path
+  end
+
+  test "should not get edit with invalid project" do
+    get :edit, id: @subject, project_id: -1
+
+    assert_nil assigns(:project)
+    assert_not_nil assigns(:subject)
+
+    assert_redirected_to root_path
   end
 
   test "should update subject" do
@@ -103,11 +156,32 @@ class SubjectsControllerTest < ActionController::TestCase
     assert_redirected_to project_subjects_path
   end
 
+  test "should not update subject with invalid project" do
+    put :update, id: @subject, project_id: -1, subject: { subject_code: @subject.subject_code, status: @subject.status }, site_id: @subject.site_id
+    assert_nil assigns(:project)
+    assert_not_nil assigns(:subject)
+    assert_redirected_to root_path
+  end
+
   test "should destroy subject" do
     assert_difference('Subject.current.count', -1) do
       delete :destroy, id: @subject, project_id: @project
     end
 
+    assert_not_nil assigns(:subject)
+    assert_not_nil assigns(:project)
+
     assert_redirected_to project_subjects_path
+  end
+
+  test "should not destroy subject with invalid project" do
+    assert_difference('Subject.current.count', 0) do
+      delete :destroy, id: @subject, project_id: -1
+    end
+
+    assert_not_nil assigns(:subject)
+    assert_nil assigns(:project)
+
+    assert_redirected_to root_path
   end
 end
