feeb37ea1d71dd8e4affb2f57b7118e18837a65d
Remo Mueller <remosm@gmail.com>
2013-01-08 16:03:20 -0500

    Version 0.11.5 finalized

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -3,7 +3,7 @@ module Slice
     MAJOR = 0
     MINOR = 11
     TINY = 5
-    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
