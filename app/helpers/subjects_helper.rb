f2a9fc6621202d6eaec834e1f8cf911fc049ff28
Remo Mueller <remosm@gmail.com>
2013-01-14 17:31:32 -0500

    Subject status is now viewable on sheets

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,2 +1,10 @@
 module SubjectsHelper
+  def status_helper(subject, long = false)
+    codes = { 'valid' => 'success', 'test' => 'info' }
+    content_tag(  :span, (long ? subject.status : subject.status[0]),
+                  class: "label label-#{codes[subject.status.to_s] || 'warning'}",
+                  rel: 'tooltip',
+                  data: { placement: 'left' },
+                  title: "#{subject.status unless long}" )
+  end
 end
