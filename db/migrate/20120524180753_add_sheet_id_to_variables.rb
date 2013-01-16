dee155bf43e2c24933f1f6a176de1e985046861b
Remo Mueller <remosm@gmail.com>
2013-01-16 15:15:55 -0500

    Updated Reverse migration for Variable index

--- db/migrate/20120524180753_add_sheet_id_to_variables.rb
+++ db/migrate/20120524180753_add_sheet_id_to_variables.rb
@@ -1,6 +1,11 @@
 class AddSheetIdToVariables < ActiveRecord::Migration
-  def change
+  def up
     add_column :variables, :sheet_id, :integer
     add_index :variables, :sheet_id
+  end
+
+  def down
+    remove_index :variables, :sheet_id
+    remove_column :variables, :sheet_id
   end
 end
