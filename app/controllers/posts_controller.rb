cd628bb3f1428c9ad7a86b91c0207fc4dce8dd3b
Remo Mueller <remosm@gmail.com>
2013-02-21 13:05:36 -0500

    Refactored posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,37 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_editable_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :redirect_without_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :set_editable_post, only: [ :show, :edit, :update, :destroy ]
+  before_filter :redirect_without_post, only: [ :show, :edit, :update, :destroy ]
+
+
   # GET /posts
   # GET /posts.json
   def index
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-
-    if @project
-      post_scope = @project.posts.scoped()
-      @order = scrub_order(Post, params[:order], "posts.name")
-      post_scope = post_scope.order(@order)
-      @post_count = post_scope.count
-      @posts = post_scope.page(params[:page]).per( 20 )
-    end
+    @order = scrub_order(Post, params[:order], "posts.name")
+    @posts = @project.posts.search(params[:search]).order(@order).page(params[:page]).per( 20 )
 
     respond_to do |format|
-      if @project
-        format.html # index.html.erb
-        format.js
-        format.json { render json: @posts }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # index.html.erb
+      format.js
+      format.json { render json: @posts }
     end
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @post = @project.posts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # show.html.erb
-        format.json { render json: @post }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # show.html.erb
+      format.json { render json: @post }
     end
   end
 
   # GET /posts/new
   # GET /posts/new.json
   def new
-    @post = Post.new(project_id: params[:project_id])
+    @post = @project.posts.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -55,38 +41,24 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @post = @project.posts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # edit.html.erb
-        format.json { render json: @post }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # edit.html.erb
+      format.json { render json: @post }
     end
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @post = @project.posts.new(post_params) if @project
+    @post = @project.posts.new(post_params)
 
     respond_to do |format|
-      if @project
-        if @post.save
-          format.html { redirect_to [@post.project, @post], notice: 'Post was successfully created.' }
-          format.json { render json: @post, status: :created, location: @post }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
+      if @post.save
+        format.html { redirect_to [@post.project, @post], notice: 'Post was successfully created.' }
+        format.json { render json: @post, status: :created, location: @post }
       else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
+        format.html { render action: "new" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -94,21 +66,13 @@ class PostsController < ApplicationController
   # PUT /posts/1
   # PUT /posts/1.json
   def update
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @post = @project.posts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        if @post.update_attributes(post_params)
-          format.html { redirect_to [@post.project, @post], notice: 'Post was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
-      else
-        format.html { redirect_to root_path }
+      if @post.update_attributes(post_params)
+        format.html { redirect_to [@post.project, @post], notice: 'Post was successfully updated.' }
         format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -116,18 +80,11 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @post = @project.posts.find_by_id(params[:id]) if @project
+    @post.destroy
 
     respond_to do |format|
-      if @project
-        @post.destroy
-        format.html { redirect_to project_posts_path }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html { redirect_to project_posts_path }
+      format.json { head :no_content }
     end
   end
 
@@ -142,4 +99,13 @@ class PostsController < ApplicationController
       :name, :description, :archived, :user_id
     )
   end
+
+  def set_editable_post
+    @post = @project.posts.find_by_id(params[:id])
+  end
+
+  def redirect_without_post
+    empty_response_or_root_path(project_posts_path) unless @post
+  end
+
 end
