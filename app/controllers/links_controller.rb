fdd72bd1caad4906b7ce9084cb1c130e857951fd
Remo Mueller <remosm@gmail.com>
2013-02-21 12:57:00 -0500

    Refactored links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,53 +1,36 @@
 class LinksController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_editable_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :redirect_without_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :set_editable_link, only: [ :show, :edit, :update, :destroy ]
+  before_filter :redirect_without_link, only: [ :show, :edit, :update, :destroy ]
 
   # GET /links
   # GET /links.json
   def index
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-
-    if @project
-      link_scope = @project.links.scoped()
-      @order = scrub_order(Link, params[:order], "links.name")
-      link_scope = link_scope.order(@order)
-      @link_count = link_scope.count
-      @links = link_scope.page(params[:page]).per( 20 )
-    end
+    @order = scrub_order(Link, params[:order], "links.name")
+    @links = @project.links.search(params[:search]).order(@order).page(params[:page]).per( 20 )
 
     respond_to do |format|
-      if @project
-        format.html # index.html.erb
-        format.js
-        format.json { render json: @links }
-      else
-        format.html { redirect_to root_path }
-        format.js { render nothing: true }
-        format.json { head :no_content }
-      end
+      format.html # index.html.erb
+      format.js
+      format.json { render json: @links }
     end
   end
 
   # GET /links/1
   # GET /links/1.json
   def show
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @link = @project.links.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # show.html.erb
-        format.json { render json: @link }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # show.html.erb
+      format.json { render json: @link }
     end
   end
 
   # GET /links/new
   # GET /links/new.json
   def new
-    @link = Link.new(project_id: params[:project_id])
+    @link = @project.links.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -57,38 +40,24 @@ class LinksController < ApplicationController
 
   # GET /links/1/edit
   def edit
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @link = @project.links.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # edit.html.erb
-        format.json { render json: @link }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # edit.html.erb
+      format.json { render json: @link }
     end
   end
 
   # POST /links
   # POST /links.json
   def create
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @link = @project.links.new(post_params) if @project
+    @link = @project.links.new(post_params)
 
     respond_to do |format|
-      if @project
-        if @link.save
-          format.html { redirect_to [@link.project, @link], notice: 'Link was successfully created.' }
-          format.json { render json: @link, status: :created, location: @link }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @link.errors, status: :unprocessable_entity }
-        end
+      if @link.save
+        format.html { redirect_to [@link.project, @link], notice: 'Link was successfully created.' }
+        format.json { render json: @link, status: :created, location: @link }
       else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
+        format.html { render action: "new" }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -96,21 +65,13 @@ class LinksController < ApplicationController
   # PUT /links/1
   # PUT /links/1.json
   def update
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @link = @project.links.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        if @link.update_attributes(post_params)
-          format.html { redirect_to [@link.project, @link], notice: 'Link was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @link.errors, status: :unprocessable_entity }
-        end
-      else
-        format.html { redirect_to root_path }
+      if @link.update_attributes(post_params)
+        format.html { redirect_to [@link.project, @link], notice: 'Link was successfully updated.' }
         format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -118,18 +79,11 @@ class LinksController < ApplicationController
   # DELETE /links/1
   # DELETE /links/1.json
   def destroy
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @link = @project.links.find_by_id(params[:id]) if @project
+    @link.destroy
 
     respond_to do |format|
-      if @project
-        @link.destroy
-        format.html { redirect_to project_links_path }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html { redirect_to project_links_path }
+      format.json { head :no_content }
     end
   end
 
@@ -144,7 +98,12 @@ class LinksController < ApplicationController
       :name, :category, :url, :archived, :user_id
     )
   end
+
+  def set_editable_link
+    @link = @project.links.find_by_id(params[:id])
+  end
+
+  def redirect_without_link
+    empty_response_or_root_path(project_links_path) unless @link
+  end
 end
-
-
-
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,7 @@ class Link < ActiveRecord::Base
   include Deletable
 
   # Named Scopes
+  scope :search, lambda { |arg| where("LOWER(links.name) LIKE ? or LOWER(links.category) LIKE ?", arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
 
   # Model Validation
   validates_presence_of :name, :category, :url, :project_id, :user_id
