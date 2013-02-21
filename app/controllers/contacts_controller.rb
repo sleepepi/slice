edbb7fbee0604f951fe7b77984db6e79182208e8
Remo Mueller <remosm@gmail.com>
2013-02-21 11:41:36 -0500

    Refactored contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,52 +1,37 @@
 class ContactsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_editable_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :redirect_without_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :set_editable_contact, only: [ :show, :edit, :update, :destroy ]
+  before_filter :redirect_without_contact, only: [ :show, :edit, :update, :destroy ]
+
 
   # GET /contacts
   # GET /contacts.json
   def index
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-
-    if @project
-      contact_scope = @project.contacts.scoped()
-      @order = scrub_order(Contact, params[:order], "contacts.name")
-      contact_scope = contact_scope.order(@order)
-      @contact_count = contact_scope.count
-      @contacts = contact_scope.page(params[:page]).per( 20 )
-    end
+    @order = scrub_order(Contact, params[:order], "contacts.name")
+    @contacts = @project.contacts.search(params[:search]).order(@order).page(params[:page]).per( 20 )
 
     respond_to do |format|
-      if @project
-        format.html # index.html.erb
-        format.js
-        format.json { render json: @contacts }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # index.html.erb
+      format.js
+      format.json { render json: @contacts }
     end
   end
 
   # GET /contacts/1
   # GET /contacts/1.json
   def show
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @contact = @project.contacts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # show.html.erb
-        format.json { render json: @contact }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # show.html.erb
+      format.json { render json: @contact }
     end
   end
 
   # GET /contacts/new
   # GET /contacts/new.json
   def new
-    @contact = Contact.new(project_id: params[:project_id])
+    @contact = @project.contacts.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -56,38 +41,24 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1/edit
   def edit
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @contact = @project.contacts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # edit.html.erb
-        format.json { render json: @contact }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # edit.html.erb
+      format.json { render json: @contact }
     end
   end
 
   # POST /contacts
   # POST /contacts.json
   def create
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @contact = @project.contacts.new(post_params) if @project
+    @contact = @project.contacts.new(post_params)
 
     respond_to do |format|
-      if @project
-        if @contact.save
-          format.html { redirect_to [@contact.project, @contact], notice: 'Contact was successfully created.' }
-          format.json { render json: @contact, status: :created, location: @contact }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @contact.errors, status: :unprocessable_entity }
-        end
+      if @contact.save
+        format.html { redirect_to [@contact.project, @contact], notice: 'Contact was successfully created.' }
+        format.json { render json: @contact, status: :created, location: @contact }
       else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
+        format.html { render action: "new" }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -95,21 +66,13 @@ class ContactsController < ApplicationController
   # PUT /contacts/1
   # PUT /contacts/1.json
   def update
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @contact = @project.contacts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        if @contact.update_attributes(post_params)
-          format.html { redirect_to [@contact.project, @contact], notice: 'Contact was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @contact.errors, status: :unprocessable_entity }
-        end
-      else
-        format.html { redirect_to root_path }
+      if @contact.update_attributes(post_params)
+        format.html { redirect_to [@contact.project, @contact], notice: 'Contact was successfully updated.' }
         format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -117,18 +80,10 @@ class ContactsController < ApplicationController
   # DELETE /contacts/1
   # DELETE /contacts/1.json
   def destroy
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @contact = @project.contacts.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        @contact.destroy
-        format.html { redirect_to project_contacts_path }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      @contact.destroy
+      format.html { redirect_to project_contacts_path }
+      format.json { head :no_content }
     end
   end
 
@@ -144,4 +99,13 @@ class ContactsController < ApplicationController
       :title, :name, :phone, :fax, :email, :position, :user_id, :archived
     )
   end
+
+  def set_editable_contact
+    @contact = @project.contacts.find_by_id(params[:id])
+  end
+
+  def redirect_without_contact
+    empty_response_or_root_path(project_contacts_path) unless @contact
+  end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,7 @@ class Contact < ActiveRecord::Base
   include Deletable
 
   # Named Scopes
+  scope :search, lambda { |arg| where("LOWER(contacts.name) LIKE ? or LOWER(contacts.title) LIKE ?", arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
 
   # Model Validation
   validates_presence_of :title, :name, :project_id, :user_id
