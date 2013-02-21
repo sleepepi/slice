35a20d14d492afcdc4da3d387449a758f262f277
Remo Mueller <remosm@gmail.com>
2013-02-21 11:33:22 -0500

    Fixed searching across joins with ambiguous names/descriptions

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -2,7 +2,7 @@ module Searchable
   extend ActiveSupport::Concern
 
   included do
-    scope :search, lambda { |arg| where('LOWER(name) LIKE ? or LOWER(description) LIKE ?', arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
+    scope :search, lambda { |arg| where("LOWER(#{self.table_name}.name) LIKE ? or LOWER(#{self.table_name}.description) LIKE ?", arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
   end
 
 end
