9f03479be2c510d3eceaa80886b68b65afe9ae4e
Remo Mueller <remosm@gmail.com>
2013-02-21 12:38:42 -0500

    Refactored documents controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,53 +1,37 @@
 class DocumentsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_editable_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :redirect_without_project, only: [ :index, :show, :new, :edit, :create, :update, :destroy ]
+  before_filter :set_editable_document, only: [ :show, :edit, :update, :destroy ]
+  before_filter :redirect_without_document, only: [ :show, :edit, :update, :destroy ]
+
 
   # GET /documents
   # GET /documents.json
   def index
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-
-    if @project
-      document_scope = @project.documents.scoped()
-      @order = scrub_order(Document, params[:order], "documents.name")
-      document_scope = document_scope.order(@order)
-      @document_count = document_scope.count
-      @documents = document_scope.page(params[:page]).per( 20 )
-    end
+    @order = scrub_order(Document, params[:order], "documents.name")
+    @documents = @project.documents.search(params[:search]).order(@order).page(params[:page]).per( 20 )
 
     respond_to do |format|
-      if @project
-        format.html # index.html.erb
-        format.js
-        format.json { render json: @documents }
-      else
-        format.html { redirect_to root_path }
-        format.js { render nothing: true }
-        format.json { head :no_content }
-      end
+      format.html # index.html.erb
+      format.js
+      format.json { render json: @documents }
     end
   end
 
   # GET /documents/1
   # GET /documents/1.json
   def show
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @document = @project.documents.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # show.html.erb
-        format.json { render json: @document }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # show.html.erb
+      format.json { render json: @document }
     end
   end
 
   # GET /documents/new
   # GET /documents/new.json
   def new
-    @document = Document.new(project_id: params[:project_id])
+    @document = @project.documents.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -57,38 +41,24 @@ class DocumentsController < ApplicationController
 
   # GET /documents/1/edit
   def edit
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @document = @project.documents.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        format.html # edit.html.erb
-        format.json { render json: @document }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      format.html # edit.html.erb
+      format.json { render json: @document }
     end
   end
 
   # POST /documents
   # POST /documents.json
   def create
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @document = @project.documents.new(post_params) if @project
+    @document = @project.documents.new(post_params)
 
     respond_to do |format|
-      if @project
-        if @document.save
-          format.html { redirect_to [@document.project, @document], notice: 'Document was successfully created.' }
-          format.json { render json: @document, status: :created, location: @document }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @document.errors, status: :unprocessable_entity }
-        end
+      if @document.save
+        format.html { redirect_to [@document.project, @document], notice: 'Document was successfully created.' }
+        format.json { render json: @document, status: :created, location: @document }
       else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
+        format.html { render action: "new" }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -96,21 +66,13 @@ class DocumentsController < ApplicationController
   # PUT /documents/1
   # PUT /documents/1.json
   def update
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @document = @project.documents.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        if @document.update_attributes(post_params)
-          format.html { redirect_to [@document.project, @document], notice: 'Document was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @document.errors, status: :unprocessable_entity }
-        end
-      else
-        format.html { redirect_to root_path }
+      if @document.update_attributes(post_params)
+        format.html { redirect_to [@document.project, @document], notice: 'Document was successfully updated.' }
         format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -118,18 +80,10 @@ class DocumentsController < ApplicationController
   # DELETE /documents/1
   # DELETE /documents/1.json
   def destroy
-    @project = current_user.all_projects.find_by_id(params[:project_id])
-    @document = @project.documents.find_by_id(params[:id]) if @project
-
     respond_to do |format|
-      if @project
-        @document.destroy
-        format.html { redirect_to project_documents_path }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
+      @document.destroy
+      format.html { redirect_to project_documents_path }
+      format.json { head :no_content }
     end
   end
 
@@ -144,4 +98,13 @@ class DocumentsController < ApplicationController
       :name, :category, :file, :file_cache, :archived, :user_id
     )
   end
+
+  def set_editable_document
+    @document = @project.documents.find_by_id(params[:id])
+  end
+
+  def redirect_without_document
+    empty_response_or_root_path(project_documents_path) unless @document
+  end
+
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -7,6 +7,7 @@ class Document < ActiveRecord::Base
   include Deletable
 
   # Named Scopes
+  scope :search, lambda { |arg| where("LOWER(documents.name) LIKE ? or LOWER(documents.category) LIKE ?", arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
 
   # Model Validation
   validates_presence_of :name, :category, :file, :project_id, :user_id
