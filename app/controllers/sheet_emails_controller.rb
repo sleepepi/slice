31963b9d66289f19df97e9576d479d471592276b
Remo Mueller <remosm@gmail.com>
2013-02-27 18:08:55 -0500

    Updated Sheet Emails

--- app/controllers/sheet_emails_controller.rb
+++ app/controllers/sheet_emails_controller.rb
@@ -5,21 +5,19 @@ class SheetEmailsController < ApplicationController
   before_action :set_viewable_sheet_email
   before_action :redirect_without_sheet_email
 
+  # GET /sheet_emails/1
+  # GET /sheet_emails/1.json
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @sheet_email }
-    end
   end
 
   private
 
-  def set_viewable_sheet_email
-    @sheet_email = current_user.all_viewable_sheet_emails.find_by_id(params[:id])
-  end
+    def set_viewable_sheet_email
+      @sheet_email = current_user.all_viewable_sheet_emails.find_by_id(params[:id])
+    end
 
-  def redirect_without_sheet_email
-    empty_response_or_root_path(project_sheets_path(@project)) unless @sheet_email
-  end
+    def redirect_without_sheet_email
+      empty_response_or_root_path(project_sheets_path(@project)) unless @sheet_email
+    end
 
 end
--- app/models/sheet_email.rb
+++ app/models/sheet_email.rb
@@ -1,5 +1,4 @@
 class SheetEmail < ActiveRecord::Base
-  # attr_accessible :email_body, :email_cc, :email_pdf_file, :email_subject, :email_to, :sheet_id, :user_id
 
   mount_uploader :email_pdf_file, GenericUploader
 
@@ -16,4 +15,5 @@ class SheetEmail < ActiveRecord::Base
   def email_receipt
     UserMailer.sheet_receipt(self).deliver if Rails.env.production?
   end
+
 end
