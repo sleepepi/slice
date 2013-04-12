9c29a3293e837f17d407b660c6538bb0f306f37c
Remo Mueller <remosm@gmail.com>
2013-04-12 16:24:29 -0400

    Updated deprecated code

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -23,8 +23,7 @@ class Grid < ActiveRecord::Base
     old_response_ids = self.responses.collect{|r| r.id} # Could use pluck, but pluck has issues with scopes and unsaved objects
     new_responses = []
     values.select{|v| not v.blank?}.each do |value|
-      r = Response.find_or_create_by_sheet_id_and_grid_id_and_variable_id_and_value(self.sheet_variable.sheet_id, self.id, self.variable_id, value, { user_id: current_user.id })
-      new_responses << r
+      new_responses << Response.where(sheet_id: self.sheet_variable.sheet_id, grid_id: self.id, variable_id: self.variable_id, value: value).first_or_create( user_id: current_user.id )
     end
     self.responses = new_responses
     Response.where(id: old_response_ids, grid_id: nil).destroy_all
