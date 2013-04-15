db054bd2025e2d0fc7066ad141bb225aaff9ffe1
Remo Mueller <remosm@gmail.com>
2013-04-15 16:00:39 -0400

    Moved generate_export into export model and out of rake task

--- test/models/export_test.rb
+++ test/models/export_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ExportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "generate an export with a sheet_scope" do
+    exports(:two).generate_export!(projects(:one).sheets)
+    assert_equal 'ready', exports(:two).status
+  end
+
 end
