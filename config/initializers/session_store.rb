33492b27f1073d14fd4a088121ddfe2243d5aae2
Remo Mueller <remosm@gmail.com>
2013-05-07 12:28:09 -0400

    Use secure cookies in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Slice::Application.config.session_store :cookie_store, key: '_slice_session'
+Slice::Application.config.session_store :cookie_store, key: '_slice_session',
+                                                       secure: Rails.env.production?
