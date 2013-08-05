d9f25f33e511d36ef94d04d0ca9231d7aa8d4742
Remo Mueller <remosm@gmail.com>
2013-08-05 12:55:47 -0400

    Updated styling for exports for BS3

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -30,53 +30,6 @@ class ExportsController < ApplicationController
     end
   end
 
-  # # GET /exports/new
-  # # GET /exports/new.json
-  # def new
-  #   @export = current_user.exports.new
-  # end
-
-  # # GET /exports/1/edit
-  # def edit
-  # end
-
-  # # POST /exports
-  # # POST /exports.json
-  # def create
-  #   @export = current_user.exports.new(export_params)
-
-  #   respond_to do |format|
-  #     if @export.save
-  #       format.html { redirect_to @export, notice: 'Export was successfully created.' }
-  #       format.json { render action: 'show', status: :created, location: @export }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @export.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # PUT /exports/1
-  # # PUT /exports/1.json
-  # def update
-  #   @export = current_user.all_exports.find_by_id(params[:id])
-
-  #   respond_to do |format|
-  #     if @export
-  #       if @export.update(export_params)
-  #         format.html { redirect_to @export, notice: 'Export was successfully updated.' }
-  #         format.json { head :no_content }
-  #       else
-  #         format.html { render action: 'edit' }
-  #         format.json { render json: @export.errors, status: :unprocessable_entity }
-  #       end
-  #     else
-  #       format.html { redirect_to root_path }
-  #       format.json { head :no_content }
-  #     end
-  #   end
-  # end
-
   # DELETE /exports/1
   # DELETE /exports/1.json
   def destroy
