6ad6a5c22b0e21e984a0ede09a54955b1be469e0
Remo Mueller <remosm@gmail.com>
2013-11-20 11:29:05 -0500

    Pi Integration

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module Slice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Required to for PI integration
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
   end
 end
