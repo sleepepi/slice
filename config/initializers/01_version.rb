e9b74560852e989cdc2113002647bd39bb6eb441
Remo Mueller <remosm@gmail.com>
2014-05-28 10:16:18 -0400

    Version bump to 0.24.10.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -2,8 +2,8 @@ module Slice
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 24
-    TINY = 9
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    TINY = 10
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
