0dda93b8b6ed9e3373c8d87fe4370ca8387e1623
Remo Mueller <remosm@gmail.com>
2014-11-19 12:12:40 -0500

    Default format for date variables is now mm/dd/yyyy

--- lib/valuables/date_response.rb
+++ lib/valuables/date_response.rb
@@ -5,7 +5,7 @@ module Valuables
   class DateResponse < Default
 
     def name
-      Date.parse(@object.response).strftime(@object.variable.format.blank? ? "%Y-%m-%d" : @object.variable.format ) rescue @object.response
+      Date.parse(@object.response).strftime(@object.variable.format.blank? ? "%m/%d/%Y" : @object.variable.format ) rescue @object.response
     end
 
   end
