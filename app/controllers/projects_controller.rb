5c54f7d896aae19343094bf9dc0c4313c1ecee50
Remo Mueller <remosm@gmail.com>
2015-04-13 14:53:58 -0400

    Added new submenu to project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -96,11 +96,7 @@ class ProjectsController < ApplicationController
   def splash
     flash.delete(:notice)
     @projects = current_user.all_viewable_and_site_projects.by_favorite(current_user.id).order("(favorite IS NULL or favorite = 'f') ASC, name").page(params[:page]).per( 8 )
-    if @projects.total_count == 1
-      redirect_to @projects.first
-    elsif current_user.beta_enabled?
-      render layout: 'layouts/application_custom'
-    end
+    redirect_to @projects.first if @projects.total_count == 1
   end
 
   def report
@@ -136,7 +132,6 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
-    render layout: 'layouts/application_custom_full' if current_user.beta_enabled?
   end
 
   # GET /projects/1/settings
