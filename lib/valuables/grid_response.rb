1f0ef82712efb74527b7ab3a0d32fe19fd1a7fca
Remo Mueller <remosm@gmail.com>
2015-05-06 16:10:40 -0400

    Improved load time when loading sheets for editing

--- lib/valuables/grid_response.rb
+++ lib/valuables/grid_response.rb
@@ -20,9 +20,11 @@ module Valuables
 
     def grid_responses(raw_format = :raw)
       grid_responses = []
-      (0..@object.grids.pluck(:position).max.to_i).each do |position|
+      all_grids = @object.grids.to_a
+
+      (0..all_grids.collect(&:position).max.to_i).each do |position|
         @object.variable.grid_variables.each do |grid_variable|
-          grid = @object.grids.find_by_variable_id_and_position(grid_variable[:variable_id], position)
+          grid = all_grids.select{|g| g.variable_id == grid_variable[:variable_id].to_i and g.position == position}.first
           grid_responses[position] ||= {}
           grid_responses[position][grid.variable.name] = grid.get_response(raw_format) if grid
         end
