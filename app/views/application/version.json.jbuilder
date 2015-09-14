e77d169afb6575d00e3a4e94147e773fc6ed0769
Remo Mueller <remosm@gmail.com>
2015-09-14 13:46:04 -0400

    Added missing minor version attribute to version JSON API

--- app/views/application/version.json.jbuilder
+++ app/views/application/version.json.jbuilder
@@ -1,6 +1,7 @@
 json.version do
   json.string Slice::VERSION::STRING
   json.major Slice::VERSION::MAJOR
+  json.minor Slice::VERSION::MINOR
   json.tiny Slice::VERSION::TINY
   json.build Slice::VERSION::BUILD
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -23,4 +23,14 @@ class ApplicationControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get version as json" do
+    get :version, format: 'json'
+    version = JSON.parse(response.body)
+    assert_equal Slice::VERSION::STRING, version['version']['string']
+    assert_equal Slice::VERSION::MAJOR, version['version']['major']
+    assert_equal Slice::VERSION::MINOR, version['version']['minor']
+    assert_equal Slice::VERSION::TINY, version['version']['tiny']
+    assert_equal Slice::VERSION::BUILD, version['version']['build']
+    assert_response :success
+  end
 end
