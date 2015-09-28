c63798334cbc60a8c8ca2ae2035a2b539990ee1c
Remo Mueller <remosm@gmail.com>
2015-09-28 17:09:40 -0400

    Rubocop recommendation: Use single quotes for non-interpolated strings

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -3,7 +3,7 @@ module Slice
     MAJOR = 0
     MINOR = 30
     TINY = 0
-    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = 'pre' # nil, 'pre', 'beta1', 'beta2', 'rc', 'rc2'
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
