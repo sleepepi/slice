4b924f6cfcaa952a3bf1bd7b059fb648219b2349
Remo Mueller <remosm@gmail.com>
2015-10-05 15:13:12 -0400

    Minor code cleanup

--- app/helpers/sheets_helper.rb
+++ app/helpers/sheets_helper.rb
@@ -1,11 +1,10 @@
 module SheetsHelper
-
   def coverage_helper(sheet, placement = 'right')
-    content_tag(  :span, "#{sheet.percent}%",
-                  class: "label label-coverage #{sheet.coverage}",
-                  rel: 'tooltip',
-                  data: { placement: placement },
-                  title: sheet.out_of )
+    content_tag(:span, "#{sheet.percent}%",
+                class: "label label-coverage #{sheet.coverage}",
+                rel: 'tooltip',
+                data: { placement: placement },
+                title: sheet.out_of)
   end
 
   def filter_link(count, project, design, variable, value, statuses)
@@ -15,5 +14,4 @@ module SheetsHelper
       link_to_if( !count.blank?, count || '-', project_sheets_path(project, design_id: design.id, statuses: statuses), target: '_blank' )
     end
   end
-
 end
--- test/models/sheet_test.rb
+++ test/models/sheet_test.rb
@@ -1,26 +1,25 @@
 require 'test_helper'
 
 class SheetTest < ActiveSupport::TestCase
-
-  test "should hide variable only if branching logic evaluates to false" do
-    assert_equal false, sheets(:one).show_variable?("1 == 0")
+  test 'should hide variable only if branching logic evaluates to false' do
+    assert_equal false, sheets(:one).show_variable?('1 == 0')
   end
 
-  test "should show variable if branching logic is invalid" do
-    assert_equal true, sheets(:one).show_variable?("abc")
-    assert_equal true, sheets(:one).show_variable?("1/0")
+  test 'should show variable if branching logic is invalid' do
+    assert_equal true, sheets(:one).show_variable?('abc')
+    assert_equal true, sheets(:one).show_variable?('1/0')
   end
 
-  test "should get sheet coverage" do
-    assert_equal "complete",  sheets(:coverage_complete).coverage
-    assert_equal "green",     sheets(:coverage_90).coverage
-    assert_equal "yellow",    sheets(:coverage_70).coverage
-    assert_equal "orange",    sheets(:coverage_50).coverage
-    assert_equal "red",       sheets(:coverage_30).coverage
-    assert_equal "blank",     sheets(:coverage_0).coverage
+  test 'should get sheet coverage' do
+    assert_equal 'complete',  sheets(:coverage_complete).coverage
+    assert_equal 'green',     sheets(:coverage_90).coverage
+    assert_equal 'yellow',    sheets(:coverage_70).coverage
+    assert_equal 'orange',    sheets(:coverage_50).coverage
+    assert_equal 'red',       sheets(:coverage_30).coverage
+    assert_equal 'blank',     sheets(:coverage_0).coverage
   end
 
-  test "should get sheet color" do
+  test 'should get sheet color' do
     assert_equal 0, /^#([0-9abcdef]){6}$/ =~ sheets(:coverage_complete).color
     assert_equal 0, /^#([0-9abcdef]){6}$/ =~ sheets(:coverage_90).color
     assert_equal 0, /^#([0-9abcdef]){6}$/ =~ sheets(:coverage_70).color
@@ -29,7 +28,7 @@ class SheetTest < ActiveSupport::TestCase
     assert_equal 0, /^#([0-9abcdef]){6}$/ =~ sheets(:coverage_0).color
   end
 
-  test "sheet completion should be based on non hidden responses" do
+  test 'sheet completion should be based on non hidden responses' do
     assert_equal 50, sheets(:filled_out_half_visible).percent
     assert_equal 100, sheets(:filled_out_all_visible).percent
     assert_equal 100, sheets(:filled_out_entire_sheet).percent
@@ -37,8 +36,7 @@ class SheetTest < ActiveSupport::TestCase
     assert_equal 0, sheets(:hidden_response_answered).percent
   end
 
-  test "get max grids position" do
+  test 'get max grids position' do
     assert_equal 1, sheets(:has_grid).max_grids_position
   end
-
 end
