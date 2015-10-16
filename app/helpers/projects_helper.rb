6b0b56c1c727055ad55d360777f3efaeb5aaa185
Remo Mueller <remosm@gmail.com>
2015-10-16 14:52:16 -0400

    Updated project menu and breadcrumb styling for documents, links, contacts, and posts, closes #264.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,7 +3,7 @@ module ProjectsHelper
   SETUP_CONTROLLERS = %w(
     categories designs events variables domains randomization_schemes
     block_size_multipliers stratification_factors stratification_factor_options
-    treatment_arms sites
+    treatment_arms sites documents links posts contacts
   )
 
   def setup_path?
