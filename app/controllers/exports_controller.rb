92c6bcfdd0f76f95d3f716dae96e7b8866d3e5a4
Remo Mueller <remosm@gmail.com>
2016-03-08 15:41:53 -0500

    Refactored exports controller

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
+# Allows a user to manage project data exports.
 class ExportsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_viewable_project,      only: [:index, :show, :new, :create, :file, :progress, :mark_unread, :destroy]
-  before_action :redirect_without_project,  only: [:index, :show, :new, :create, :file, :progress, :mark_unread, :destroy]
-  before_action :set_viewable_export,       only: [:show, :file, :mark_unread, :progress]
-  before_action :set_editable_export,       only: [:destroy]
-  before_action :redirect_without_export,   only: [:show, :file, :mark_unread, :progress, :destroy]
+  before_action :find_viewable_project_or_redirect
+  before_action :find_viewable_export_or_redirect, only: [:show, :file, :mark_unread, :progress]
+  before_action :find_editable_export_or_redirect, only: [:destroy]
 
   def file
     if @export.file.size > 0
@@ -23,9 +22,8 @@ class ExportsController < ApplicationController
   # GET /exports
   def index
     @order = scrub_order(Export, params[:order], 'exports.created_at DESC')
-    @exports = current_user.all_viewable_exports.where(project_id: @project.id)
-                           .search(params[:search]).order(@order)
-                           .page(params[:page]).per(20)
+    @exports = viewable_exports.search(params[:search]).order(@order)
+                               .page(params[:page]).per(20)
   end
 
   # GET /exports/1
@@ -33,25 +31,24 @@ class ExportsController < ApplicationController
     @export.update viewed: true if @export.status == 'ready'
   end
 
+  # GET /exports/new
   def new
     @export = current_user.exports.where(project_id: @project.id).new
   end
 
+  # POST /exports/1/mark_unread
   def mark_unread
     @export.update viewed: false
     redirect_to project_exports_path(@project)
   end
 
+  # POST /exports
   def create
-    name = "#{@project.name.gsub(/[^a-zA-Z0-9_]/, '_')}_#{Time.zone.today.strftime('%Y%m%d')}"
-    @export = current_user.exports.where(project_id: @project.id, name: name, total_steps: 1).create(export_params)
+    @export = current_user.exports
+                          .where(project_id: @project.id, name: export_name, total_steps: 1)
+                          .create(export_params)
     @export.generate_export_in_background!
-
-    if @export.new_record?
-      redirect_to project_exports_path(@project)
-    else
-      redirect_to [@project, @export]
-    end
+    redirect_to [@project, @export]
   end
 
   # DELETE /exports/1
@@ -62,12 +59,18 @@ class ExportsController < ApplicationController
 
   private
 
-  def set_viewable_export
-    @export = current_user.all_viewable_exports.find_by_id(params[:id])
+  def viewable_exports
+    current_user.all_viewable_exports.where(project_id: @project.id)
   end
 
-  def set_editable_export
-    @export = current_user.all_exports.find_by_id(params[:id])
+  def find_viewable_export_or_redirect
+    @export = viewable_exports.find_by_id params[:id]
+    redirect_without_export
+  end
+
+  def find_editable_export_or_redirect
+    @export = current_user.all_exports.where(project_id: @project.id).find_by_id params[:id]
+    redirect_without_export
   end
 
   def redirect_without_export
@@ -75,9 +78,13 @@ class ExportsController < ApplicationController
   end
 
   def export_params
-    params.require(:export).permit(:include_csv_labeled, :include_csv_raw,
-                                   :include_pdf, :include_files,
-                                   :include_data_dictionary,
-                                   :include_sas, :include_r)
+    params.require(:export).permit(
+      :include_csv_labeled, :include_csv_raw, :include_pdf, :include_files,
+      :include_data_dictionary, :include_sas, :include_r
+    )
+  end
+
+  def export_name
+    "#{@project.name.gsub(/[^a-zA-Z0-9_]/, '_')}_#{Time.zone.today.strftime('%Y%m%d')}"
   end
 end
