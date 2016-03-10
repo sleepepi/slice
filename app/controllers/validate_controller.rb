da590056eb8da16083939332563c1f263f627521
Remo Mueller <remosm@gmail.com>
2016-03-10 12:56:13 -0500

    Refactored validate controller

--- app/controllers/validate_controller.rb
+++ app/controllers/validate_controller.rb
@@ -2,21 +2,22 @@
 
 # Allows variables on project to be validated
 class ValidateController < ApplicationController
-  before_action :set_project
-  before_action :set_variable
+  before_action :find_project_or_redirect
+  before_action :find_variable_or_redirect
 
+  # POST /validate/variable.json
   def variable
     render json: @variable.value_in_range?(params[:value])
   end
 
   private
 
-  def set_project
+  def find_project_or_redirect
     @project = Project.current.find_by_param params[:project_id]
     empty_response_or_root_path unless @project
   end
 
-  def set_variable
+  def find_variable_or_redirect
     @variable = @project.variables.find_by_id params[:variable_id]
     empty_response_or_root_path unless @variable
   end
