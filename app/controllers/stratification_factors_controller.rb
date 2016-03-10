0fa7486bf062dc1497f2cbf043a6c309a7a7a811
Remo Mueller <remosm@gmail.com>
2016-03-10 11:15:43 -0500

    Refactored stratification factors controller

--- app/controllers/stratification_factors_controller.rb
+++ app/controllers/stratification_factors_controller.rb
@@ -4,14 +4,11 @@
 # randomization scheme
 class StratificationFactorsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_editable_project
-  before_action :redirect_without_project
+  before_action :find_editable_project_or_redirect
   before_action :redirect_blinded_users
-  before_action :set_randomization_scheme
-  before_action :redirect_without_randomization_scheme
-  before_action :set_stratification_factor,               only: [:show, :edit, :update, :destroy]
-  before_action :redirect_without_stratification_factor,  only: [:show, :edit, :update, :destroy]
-  before_action :redirect_with_published_scheme,          only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_randomization_scheme_or_redirect
+  before_action :find_stratification_factor_or_redirect, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_with_published_scheme, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /stratification_factors
   def index
@@ -35,9 +32,13 @@ class StratificationFactorsController < ApplicationController
   # POST /stratification_factors
   def create
     @stratification_factor = @randomization_scheme.stratification_factors
-                                                  .where(project_id: @project.id, user_id: current_user.id).new(stratification_factor_params)
+                                                  .where(project_id: @project.id, user_id: current_user.id)
+                                                  .new(stratification_factor_params)
     if @stratification_factor.save
-      redirect_to [@project, @randomization_scheme, @stratification_factor], notice: 'Stratification factor was successfully created.'
+      redirect_to(
+        [@project, @randomization_scheme, @stratification_factor],
+        notice: 'Stratification factor was successfully created.'
+      )
     else
       render :new
     end
@@ -46,7 +47,10 @@ class StratificationFactorsController < ApplicationController
   # PATCH /stratification_factors/1
   def update
     if @stratification_factor.update(stratification_factor_params)
-      redirect_to [@project, @randomization_scheme, @stratification_factor], notice: 'Stratification factor was successfully updated.'
+      redirect_to(
+        [@project, @randomization_scheme, @stratification_factor],
+        notice: 'Stratification factor was successfully updated.'
+      )
     else
       render :edit
     end
@@ -55,38 +59,56 @@ class StratificationFactorsController < ApplicationController
   # DELETE /stratification_factors/1
   def destroy
     @stratification_factor.destroy
-    redirect_to project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme), notice: 'Stratification factor was successfully deleted.'
+    redirect_to(
+      project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme),
+      notice: 'Stratification factor was successfully deleted.'
+    )
   end
 
   private
 
-  def set_randomization_scheme
+  def find_randomization_scheme_or_redirect
     @randomization_scheme = @project.randomization_schemes.find_by_id(params[:randomization_scheme_id])
+    redirect_without_randomization_scheme
   end
 
   def redirect_without_randomization_scheme
-    empty_response_or_root_path(project_randomization_schemes_path(@project)) unless @randomization_scheme
+    return if @randomization_scheme
+    empty_response_or_root_path(project_randomization_schemes_path(@project))
   end
 
-  def set_stratification_factor
+  def find_stratification_factor_or_redirect
     @stratification_factor = @randomization_scheme.stratification_factors.find_by_id(params[:id])
+    redirect_without_stratification_factor
   end
 
   def redirect_without_stratification_factor
-    empty_response_or_root_path(project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme)) unless @stratification_factor
+    return if @stratification_factor
+    empty_response_or_root_path(
+      project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme)
+    )
   end
 
   def redirect_with_published_scheme
     if @randomization_scheme.published?
       flash[:alert] = "Stratification factors can't be created or edited on published randomization scheme."
-      empty_response_or_root_path(@stratification_factor ? [@project, @randomization_scheme, @stratification_factor] : project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme))
+      url = if @stratification_factor
+              [@project, @randomization_scheme, @stratification_factor]
+            else
+              project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme)
+            end
+      empty_response_or_root_path(url)
     end
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def stratification_factor_params
     params[:stratification_factor] ||= { blank: '1' }
-    params[:stratification_factor][:calculation] = params[:stratification_factor][:calculation].to_s.strip if params[:stratification_factor].key?(:calculation)
+    clean_calculation
     params.require(:stratification_factor).permit(:name, :stratifies_by_site, :calculation)
+  end
+
+  def clean_calculation
+    return unless params[:stratification_factor].key?(:calculation)
+    params[:stratification_factor][:calculation] = params[:stratification_factor][:calculation].to_s.strip
   end
 end
--- test/controllers/stratification_factors_controller_test.rb
+++ test/controllers/stratification_factors_controller_test.rb
@@ -2,89 +2,172 @@
 
 require 'test_helper'
 
+# Allows project editors to modify stratification factors for randomization
+# schemes.
 class StratificationFactorsControllerTest < ActionController::TestCase
   setup do
-    login(users(:valid))
+    @project_editor = users(:project_one_editor)
     @project = projects(:one)
-
     @published_scheme = randomization_schemes(:one)
     @published_stratification_factor = stratification_factors(:gender)
-
     @randomization_scheme = randomization_schemes(:two)
     @stratification_factor = stratification_factors(:weight)
   end
 
-  test "should get index" do
+  def sf_params
+    {
+      name: @stratification_factor.name,
+      stratifies_by_site: @stratification_factor.stratifies_by_site,
+      calculation: @stratification_factor.calculation
+    }
+  end
+
+  test 'should get index' do
+    login(@project_editor)
     get :index, project_id: @project, randomization_scheme_id: @randomization_scheme
     assert_response :success
     assert_not_nil assigns(:stratification_factors)
   end
 
-  test "should get new" do
+  test 'should get new' do
+    login(@project_editor)
     get :new, project_id: @project, randomization_scheme_id: @randomization_scheme
     assert_response :success
   end
 
-  test "should not get new for published randomization scheme" do
+  test 'should not get new for published randomization scheme' do
+    login(@project_editor)
     get :new, project_id: @project, randomization_scheme_id: @published_scheme
-    assert_redirected_to project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+    )
   end
 
-  test "should create stratification_factor" do
+  test 'should create stratification factor' do
+    login(@project_editor)
     assert_difference('StratificationFactor.count') do
-      post :create, project_id: @project, randomization_scheme_id: @randomization_scheme, stratification_factor: { name: "New Stratification Factor" }
+      post :create, project_id: @project, randomization_scheme_id: @randomization_scheme,
+                    stratification_factor: sf_params.merge(name: 'New Stratification Factor')
     end
 
-    assert_redirected_to project_randomization_scheme_stratification_factor_path(assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor))
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_path(
+        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      )
+    )
   end
 
-  test "should not create stratification factor for published randomization scheme" do
+  test 'should not create stratification factor with blank name' do
+    login(@project_editor)
     assert_difference('StratificationFactor.count', 0) do
-      post :create, project_id: @project, randomization_scheme_id: @published_scheme, stratification_factor: { name: "New Stratification Factor" }
+      post :create, project_id: @project, randomization_scheme_id: @randomization_scheme,
+                    stratification_factor: sf_params.merge(name: '')
     end
-
-    assert_redirected_to project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+    assert_template 'new'
+    assert_response :success
   end
 
-  test "should show stratification_factor" do
+  test 'should not create stratification factor for published randomization scheme' do
+    login(@project_editor)
+    assert_difference('StratificationFactor.count', 0) do
+      post :create, project_id: @project, randomization_scheme_id: @published_scheme,
+                    stratification_factor: sf_params.merge(name: 'New Stratification Factor')
+    end
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+    )
+  end
+
+  test 'should show stratification factor' do
+    login(@project_editor)
     get :show, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should not show stratification factor with invalid randomization scheme' do
+    login(@project_editor)
+    get :show, project_id: @project, randomization_scheme_id: -1, id: @stratification_factor
+    assert_redirected_to project_randomization_schemes_path(@project)
+  end
+
+  test 'should not show stratification factor with invalid stratification factor' do
+    login(@project_editor)
+    get :show, project_id: @project, randomization_scheme_id: @randomization_scheme, id: -1
+    assert_redirected_to project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme)
+  end
+
+  test 'should get edit' do
+    login(@project_editor)
     get :edit, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor
     assert_response :success
   end
 
-  test "should not get edit for published randomization scheme" do
+  test 'should not get edit for published randomization scheme' do
+    login(@project_editor)
     get :edit, project_id: @project, randomization_scheme_id: @published_scheme, id: @published_stratification_factor
-    assert_redirected_to project_randomization_scheme_stratification_factor_path(assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor))
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_path(
+        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      )
+    )
   end
 
-  test "should update stratification_factor" do
-    patch :update, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor, stratification_factor: { name: "Updated Stratification Factor" }
-    assert_redirected_to project_randomization_scheme_stratification_factor_path(assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor))
+  test 'should update stratification factor' do
+    login(@project_editor)
+    patch :update, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor,
+                   stratification_factor: sf_params.merge(name: 'Updated Stratification Factor')
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_path(
+        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      )
+    )
   end
 
-  test "should not update stratification factor for published randomization scheme" do
-    patch :update, project_id: @project, randomization_scheme_id: @published_scheme, id: @published_stratification_factor, stratification_factor: { name: "Updated Stratification Factor" }
-    assert_equal "Gender", assigns(:stratification_factor).name
-    assert_redirected_to project_randomization_scheme_stratification_factor_path(assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor))
+  test 'should update stratification factor with blank name' do
+    login(@project_editor)
+    patch :update, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor,
+                   stratification_factor: sf_params.merge(name: '')
+    assert_template 'edit'
+    assert_response :success
   end
 
-  test "should destroy stratification_factor" do
+  test 'should not update stratification factor for published randomization scheme' do
+    login(@project_editor)
+    patch :update, project_id: @project, randomization_scheme_id: @published_scheme,
+                   id: @published_stratification_factor,
+                   stratification_factor: sf_params.merge(name: 'Updated Stratification Factor')
+    assert_equal 'Gender', assigns(:stratification_factor).name
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_path(
+        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      )
+    )
+  end
+
+  test 'should destroy stratification factor' do
+    login(@project_editor)
     assert_difference('StratificationFactor.current.count', -1) do
       delete :destroy, project_id: @project, randomization_scheme_id: @randomization_scheme, id: @stratification_factor
     end
 
-    assert_redirected_to project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factors_path(
+        assigns(:project), assigns(:randomization_scheme)
+      )
+    )
   end
 
-  test "should not destroy stratification factor for published randomization scheme" do
+  test 'should not destroy stratification factor for published randomization scheme' do
+    login(@project_editor)
     assert_difference('StratificationFactor.current.count', 0) do
-      delete :destroy, project_id: @project, randomization_scheme_id: @published_scheme, id: @published_stratification_factor
+      delete :destroy, project_id: @project, randomization_scheme_id: @published_scheme,
+                       id: @published_stratification_factor
     end
 
-    assert_redirected_to project_randomization_scheme_stratification_factor_path(assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor))
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_path(
+        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      )
+    )
   end
 end
