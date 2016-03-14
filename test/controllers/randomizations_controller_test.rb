7eac27247843198092409468b8233570ecb0cefd
Remo Mueller <remosm@gmail.com>
2016-03-14 17:46:38 -0400

    Updated tests to use Rails 5 keyword arguments

--- test/controllers/randomizations_controller_test.rb
+++ test/controllers/randomizations_controller_test.rb
@@ -10,77 +10,81 @@ class RandomizationsControllerTest < ActionController::TestCase
 
   test 'should get choose scheme and choose single published randomization scheme' do
     login(users(:valid))
-    get :choose_scheme, project_id: @project
+    get :choose_scheme, params: { project_id: @project }
     assert_redirected_to randomize_subject_project_randomization_scheme_path(assigns(:project), randomization_schemes(:one))
   end
 
   test 'should get choose scheme and give options to multiple published randomization schemes' do
     login(users(:valid))
-    get :choose_scheme, project_id: projects(:two)
+    get :choose_scheme, params: { project_id: projects(:two) }
     assert_response :success
   end
 
   test 'should get choose scheme for site editor' do
     login(users(:site_two_editor))
-    get :choose_scheme, project_id: projects(:two)
+    get :choose_scheme, params: { project_id: projects(:two) }
     assert_response :success
   end
 
   test 'should not get choose scheme for site viewer' do
     login(users(:site_two_viewer))
-    get :choose_scheme, project_id: projects(:two)
+    get :choose_scheme, params: { project_id: projects(:two) }
     assert_redirected_to root_path
   end
 
   test 'should get index' do
     login(users(:valid))
-    get :index, project_id: @project
+    get :index, params: { project_id: @project }
     assert_response :success
     assert_not_nil assigns(:randomizations)
   end
 
   test 'should get index for site editor' do
     login(users(:site_one_editor))
-    get :index, project_id: @project
+    get :index, params: { project_id: @project }
     assert_response :success
     assert_not_nil assigns(:randomizations)
   end
 
   test 'should get index for site viewer' do
     login(users(:site_one_viewer))
-    get :index, project_id: @project
+    get :index, params: { project_id: @project }
     assert_response :success
     assert_not_nil assigns(:randomizations)
   end
 
   test 'should show randomization' do
     login(users(:valid))
-    get :show, project_id: @project, id: @randomization
+    get :show, params: { project_id: @project, id: @randomization }
     assert_response :success
   end
 
   test 'should show randomization for site editor' do
     login(users(:site_one_editor))
-    get :show, project_id: @project, id: @randomization
+    get :show, params: { project_id: @project, id: @randomization }
     assert_response :success
   end
 
   test 'should show randomization for site viewer' do
     login(users(:site_one_viewer))
-    get :show, project_id: @project, id: @randomization
+    get :show, params: { project_id: @project, id: @randomization }
     assert_response :success
   end
 
   test 'should show randomization for minimization scheme' do
     login(users(:valid))
-    get :show, project_id: projects(:two), id: randomizations(:min_one)
+    get :show, params: {
+      project_id: projects(:two), id: randomizations(:min_one)
+    }
     assert_response :success
   end
 
   test 'should print randomization schedule for site editor' do
     skip if ENV['TRAVIS'] # Skip this test on Travis since Travis can't generate PDFs
     login(users(:site_one_editor))
-    get :schedule, project_id: @project, id: @randomization, format: 'pdf'
+    get :schedule, params: {
+      project_id: @project, id: @randomization
+    }, format: 'pdf'
     assert_not_nil assigns(:randomization)
     assert_response :success
   end
@@ -88,7 +92,9 @@ class RandomizationsControllerTest < ActionController::TestCase
   test 'should print randomization schedule for site viewer' do
     skip if ENV['TRAVIS'] # Skip this test on Travis since Travis can't generate PDFs
     login(users(:site_one_viewer))
-    get :schedule, project_id: @project, id: @randomization, format: 'pdf'
+    get :schedule, params: {
+      project_id: @project, id: @randomization
+    }, format: 'pdf'
     assert_not_nil assigns(:randomization)
     assert_response :success
   end
@@ -99,7 +105,9 @@ class RandomizationsControllerTest < ActionController::TestCase
       original_latex = ENV['latex_location']
       ENV['latex_location'] = "echo #{original_latex}"
       login(users(:site_one_editor))
-      get :schedule, project_id: @project, id: randomizations(:two), format: 'pdf'
+      get :schedule, params: {
+        project_id: @project, id: randomizations(:two)
+      }, format: 'pdf'
       assert_not_nil assigns(:randomization)
       assert_redirected_to [@project, randomizations(:two)]
     ensure
@@ -109,7 +117,7 @@ class RandomizationsControllerTest < ActionController::TestCase
 
   test 'should undo randomization' do
     login(users(:valid))
-    patch :undo, project_id: @project, id: @randomization
+    patch :undo, params: { project_id: @project, id: @randomization }
     assert_not_nil assigns(:project)
     assert_not_nil assigns(:randomization)
     assert_nil assigns(:randomization).subject_id
@@ -120,7 +128,7 @@ class RandomizationsControllerTest < ActionController::TestCase
 
   test 'should not undo randomization as site editor' do
     login(users(:site_one_editor))
-    patch :undo, project_id: @project, id: @randomization
+    patch :undo, params: { project_id: @project, id: @randomization }
     assert_not_nil assigns(:project)
     assert_nil assigns(:randomization)
     assert_redirected_to project_randomizations_path(assigns(:project))
