dffb32d56ffbe52f5131e8123b3ef0b72cabd4f6
Remo Mueller <remosm@gmail.com>
2016-06-07 16:07:31 -0400

    Temporarily removed setting shareable links for adverse events, addresses #370.

--- app/models/adverse_event_review.rb
+++ app/models/adverse_event_review.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# - **Adverse Event Changes**
+#   - Project and site editors can now generate links to send adverse event
+#     summaries to medical monitors
+#   - External adverse event reviewers can leave their name and comment on the
+#     adverse event
 class AdverseEventReview < ActiveRecord::Base
   # Model Validation
   validates :adverse_event_id, :name, :comment, presence: true
