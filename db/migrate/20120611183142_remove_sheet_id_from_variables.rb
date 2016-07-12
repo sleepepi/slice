12151e7b8f19fe14b4b08a061d785c54e37a7fea
Remo Mueller <remosm@gmail.com>
2016-07-12 09:28:11 -0400

    Fixed migration that was missing addition and removal of index

--- db/migrate/20120611183142_remove_sheet_id_from_variables.rb
+++ db/migrate/20120611183142_remove_sheet_id_from_variables.rb
@@ -1,9 +1,11 @@
 class RemoveSheetIdFromVariables < ActiveRecord::Migration[4.2]
   def up
+    remove_index :variables, :sheet_id
     remove_column :variables, :sheet_id
   end
 
   def down
     add_column :variables, :sheet_id, :integer
+    add_index :variables, :sheet_id
   end
 end
