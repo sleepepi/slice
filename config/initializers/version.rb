ab430bac5618b226d7980fa23b4dbc076ee50cab
Remo Mueller <remosm@gmail.com>
2016-09-02 11:31:24 -0400

    Version bump to 0.41.0.beta6

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module Slice
     MAJOR = 0
     MINOR = 41
     TINY = 0
-    BUILD = 'beta5' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = 'beta6' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
