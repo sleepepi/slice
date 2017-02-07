f936f1b789cd6bd34ccdff6edd019e68c004a343
Remo Mueller <remosm@gmail.com>
2017-02-07 10:28:29 -0500

    Cleaned up account controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,32 +3,20 @@
 # Allows a user to view and update account preferences.
 class AccountController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_invite_tokens, only: :dashboard
 
   # GET /dashboard
   def dashboard
-    if session[:invite_token].present?
-      redirect_to accept_project_users_path
-      return
-    elsif session[:site_invite_token].present?
-      site_invite_token = session[:site_invite_token]
-      @site_user = SiteUser.find_by_invite_token(site_invite_token)
-      if @site_user
-        redirect_to accept_project_site_users_path(@site_user.project)
-        return
-      else
-        session[:site_invite_token] = nil
-        flash[:alert] = 'Invalid invitation token.'
-      end
-    end
-
-    @projects = current_user.all_viewable_and_site_projects.by_favorite(current_user.id).unarchived.order("(favorited IS NULL or favorited = 'f') ASC, position, name").page(params[:page]).per(Project::PER_PAGE)
-
+    @projects = current_user.all_viewable_and_site_projects
+                            .by_favorite(current_user.id).unarchived
+                            .order("(favorited IS NULL or favorited = 'f') ASC, position, name")
+                            .page(params[:page]).per(Project::PER_PAGE)
     @favorited_projects = @projects.where(project_preferences: { favorited: true })
     @current_projects = @projects.where(project_preferences: { favorited: [false, nil] }).reorder('lower(name) asc')
-
     redirect_to @projects.first if current_user.all_viewable_and_site_projects.count == 1
   end
 
+  # PATCH /change_password
   def change_password
     if current_user.valid_password?(params[:user][:current_password])
       if current_user.reset_password(params[:user][:password], params[:user][:password_confirmation])
@@ -43,9 +31,11 @@ class AccountController < ApplicationController
     end
   end
 
-  def settings
-  end
+  # GET /settings
+  # def settings
+  # end
 
+  # POST /settings
   def update_settings
     current_user.update user_params
     redirect_to settings_path, notice: 'Settings saved.'
@@ -57,5 +47,19 @@ class AccountController < ApplicationController
     params.require(:user).permit(
       :first_name, :last_name, :email, :emails_enabled, :theme
     )
+  end
+
+  def check_invite_tokens
+    if session[:invite_token].present?
+      redirect_to accept_project_users_path
+    elsif session[:site_invite_token].present?
+      @site_user = SiteUser.find_by(invite_token: session[:site_invite_token])
+      if @site_user
+        redirect_to accept_project_site_users_path(@site_user.project)
+      else
+        session[:site_invite_token] = nil
+        flash[:alert] = 'Invalid invitation token.'
+      end
+    end
   end
 end
