ce933f9c66ed501bc8745cdc0dcef9413cd6e572
Remo Mueller <remosm@gmail.com>
2017-02-22 15:48:26 -0500

    Minor code style change

--- test/models/variable_test.rb
+++ test/models/variable_test.rb
@@ -5,13 +5,15 @@ require 'test_helper'
 # Tests to assure that certain variable methods work as expected.
 class VariableTest < ActiveSupport::TestCase
   test 'should get variable tooltip' do
-    assert_equal "[-100, 100] years", Variable.new(hard_minimum: -100, hard_maximum: 100, soft_minimum: 0, soft_maximum: 50, units: 'years').range_tooltip
-    assert_equal ">= -100 years",     Variable.new(hard_minimum: -100, soft_minimum: 0, units: 'years').range_tooltip
-    assert_equal "<= 100 years",      Variable.new(hard_maximum: 100, soft_maximum: 50, units: 'years').range_tooltip
-    assert_equal "",                  Variable.new(units: 'years').range_tooltip
-    assert_equal "[0, 50]",           Variable.new(soft_minimum: 0, soft_maximum: 50).range_tooltip
-    assert_equal ">= 0",              Variable.new(soft_minimum: 0).range_tooltip
-    assert_equal "<= 50",             Variable.new(soft_maximum: 50).range_tooltip
+    assert_equal '[-100, 100] years', Variable.new(
+      hard_minimum: -100, hard_maximum: 100, soft_minimum: 0, soft_maximum: 50, units: 'years'
+    ).range_tooltip
+    assert_equal '>= -100 years',     Variable.new(hard_minimum: -100, soft_minimum: 0, units: 'years').range_tooltip
+    assert_equal '<= 100 years',      Variable.new(hard_maximum: 100, soft_maximum: 50, units: 'years').range_tooltip
+    assert_equal '',                  Variable.new(units: 'years').range_tooltip
+    assert_equal '[0, 50]',           Variable.new(soft_minimum: 0, soft_maximum: 50).range_tooltip
+    assert_equal '>= 0',              Variable.new(soft_minimum: 0).range_tooltip
+    assert_equal '<= 50',             Variable.new(soft_maximum: 50).range_tooltip
   end
 
   test 'should be first scale variable if scale variable exists by itself in a grid' do
