71fc2a67410e9e18711d49b07302172d985de0e6
Remo Mueller <remosm@gmail.com>
2017-06-08 11:52:24 -0400

    Cleaned up randomization characteristic model

--- app/models/randomization_characteristic.rb
+++ app/models/randomization_characteristic.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Tracks what criteria were selected for randomization.
 class RandomizationCharacteristic < ApplicationRecord
   # Relationships
   belongs_to :project
@@ -7,6 +8,6 @@ class RandomizationCharacteristic < ApplicationRecord
   belongs_to :list
   belongs_to :randomization
   belongs_to :stratification_factor
-  belongs_to :stratification_factor_option
-  belongs_to :site
+  belongs_to :stratification_factor_option, optional: true
+  belongs_to :site, optional: true
 end
