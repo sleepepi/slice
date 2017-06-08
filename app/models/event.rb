95bcf35d838b0ee44337529a052dcf08ad27cd16
Remo Mueller <remosm@gmail.com>
2017-06-08 11:19:50 -0400

    Cleaned up event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,18 @@
 # Groups together a set of designs on a specific date.
 class Event < ApplicationRecord
   # Concerns
-  include Searchable, Deletable, Forkable, Sluggable, Blindable, Squishable
+  include Blindable
+  include Deletable
+  include Forkable
+  include Searchable
+  include Sluggable
+  include Squishable
 
   squish :name, :slug
 
   attr_accessor :design_hashes
+
+  # Callbacks
   after_save :set_event_designs, :reset_coverage
 
   # Scopes
@@ -23,7 +30,6 @@ class Event < ApplicationRecord
   belongs_to :project
   has_many :event_designs, -> { order(:position) }
   has_many :designs, -> { current }, through: :event_designs
-
   has_many :subject_events
 
   # Methods
@@ -87,12 +93,12 @@ class Event < ApplicationRecord
     event_designs.create(
       design_id: design.id, position: position,
       handoff_enabled: hash[:handoff_enabled],
-      duplicates: hash[:duplicates].present? ? hash[:duplicates] : 'highlight',
-      requirement: hash[:requirement].present? ? hash[:requirement] : 'always',
+      duplicates: hash[:duplicates].present? ? hash[:duplicates] : "highlight",
+      requirement: hash[:requirement].present? ? hash[:requirement] : "always",
       conditional_event_id: hash[:conditional_event_id],
       conditional_design_id: hash[:conditional_design_id],
       conditional_variable_id: hash[:conditional_variable_id],
-      conditional_operator: hash[:conditional_operator].present? ? hash[:conditional_operator] : '=',
+      conditional_operator: hash[:conditional_operator].present? ? hash[:conditional_operator] : "=",
       conditional_value: hash[:conditional_value]
     )
   end
