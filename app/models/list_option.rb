a18babe8dabe7754f24734e2aede316f770ed6fa
Remo Mueller <remosm@gmail.com>
2017-06-08 11:43:16 -0400

    Cleaned up list option model

--- app/models/list_option.rb
+++ app/models/list_option.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# Tracks what stratification factor options are used to be part of the list.
 class ListOption < ApplicationRecord
   # Relationships
   belongs_to :project
   belongs_to :randomization_scheme
   belongs_to :list
-  belongs_to :option, class_name: 'StratificationFactorOption', foreign_key: 'option_id'
+  belongs_to :option, class_name: "StratificationFactorOption", foreign_key: "option_id"
 
   # Methods
   def name
