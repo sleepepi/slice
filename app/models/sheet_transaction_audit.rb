e06b067024d1a51d6bcdb72df08bb636e4017ef5
Remo Mueller <remosm@gmail.com>
2017-06-08 12:05:23 -0400

    Cleaned up sheet transaction audit model

--- app/models/sheet_transaction_audit.rb
+++ app/models/sheet_transaction_audit.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+# Tracks an individual change to a sheet.
 class SheetTransactionAudit < ApplicationRecord
   # Relationships
-  belongs_to :sheet_transaction
-  belongs_to :user
   belongs_to :sheet
-  belongs_to :sheet_variable
-  belongs_to :grid
+  belongs_to :sheet_transaction
+  belongs_to :user, optional: true
+  belongs_to :sheet_variable, optional: true
+  belongs_to :grid, optional: true
 end
