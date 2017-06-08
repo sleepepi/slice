a256703c4b623c0b0b785da17579eb6b6994ebcb
Remo Mueller <remosm@gmail.com>
2017-06-08 11:30:12 -0400

    Cleaned up grid model

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -4,23 +4,24 @@
 # (row), parent grid variable, and child variable.
 class Grid < ApplicationRecord
   # Concerns
-  include Formattable, Valuable
+  include Formattable
+  include Valuable
 
   # Scopes
-  scope :with_files, -> { file_variables.where.not(response_file: [nil, '']) }
+  scope :with_files, -> { file_variables.where.not(response_file: [nil, ""]) }
 
   # Validations
   validates :sheet_variable_id, :position, presence: true
 
   # Relationships
   belongs_to :sheet_variable, touch: true
-  belongs_to :user
-  belongs_to :domain_option
+  belongs_to :user, optional: true
+  belongs_to :domain_option, optional: true
 
   delegate :sheet_id, to: :sheet_variable
 
   # Methods
   def self.file_variables
-    joins(:variable).where(variables: { variable_type: 'file' })
+    joins(:variable).where(variables: { variable_type: "file" })
   end
 end
