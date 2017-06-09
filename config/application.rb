b945cfcfb3d9845df814473a3646cb0d40cea3e2
Remo Mueller <remosm@gmail.com>
2017-06-09 10:20:12 -0400

    Upgraded to Rails 5.1 default configuration

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-# require 'action_cable/engine'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,13 +20,16 @@ Bundler.require(*Rails.groups)
 
 module Slice
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rails time:zones" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = "Eastern Time (US & Canada)"
 
     # Overwrite Rails errors to use Bootstrap CSS classes
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
@@ -34,20 +37,20 @@ module Slice
     end
 
     # Add RandomizationAlgorithm Module to autoload path
-    config.autoload_paths << Rails.root.join('app', 'models', 'randomization_algorithm')
-    config.autoload_paths << Rails.root.join('app', 'models', 'randomization_algorithm', 'algorithms')
+    config.autoload_paths << Rails.root.join("app", "models", "randomization_algorithm")
+    config.autoload_paths << Rails.root.join("app", "models", "randomization_algorithm", "algorithms")
 
     # Add Validation Module to autoload path
-    config.autoload_paths << Rails.root.join('app', 'models', 'validation')
-    config.autoload_paths << Rails.root.join('app', 'models', 'validation', 'validators')
+    config.autoload_paths << Rails.root.join("app", "models", "validation")
+    config.autoload_paths << Rails.root.join("app", "models", "validation", "validators")
 
     # Add Formatters Module to autoload path
-    config.autoload_paths << Rails.root.join('app', 'models', 'formatters')
+    config.autoload_paths << Rails.root.join("app", "models", "formatters")
 
     # Add Search Module to autoload path
-    config.autoload_paths << Rails.root.join('app', 'models', 'search')
+    config.autoload_paths << Rails.root.join("app", "models", "search")
 
     # Add Statistics Module to autoload path
-    config.autoload_paths << Rails.root.join('app', 'models', 'statistics')
+    config.autoload_paths << Rails.root.join("app", "models", "statistics")
   end
 end
