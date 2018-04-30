9f5fd3a44c85f973db240b8e523c42b3f5c09dfa
Remo Mueller <remosm@gmail.com>
2018-04-30 13:07:06 -0400

    Updated to bootstrap 4.1.1

--- gems.rb
+++ gems.rb
@@ -9,7 +9,7 @@ gem "pg",                   "1.0.0"
 
 # Gems used by project
 gem "autoprefixer-rails"
-gem "bootstrap",            "~> 4.1.0"
+gem "bootstrap",            "~> 4.1.1"
 gem "carrierwave",          "~> 1.2.2"
 gem "chunky_png",           "~> 1.3.4"
 gem "colorize",             "~> 0.8.1"
