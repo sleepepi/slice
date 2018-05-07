d91fe087873357babfab5dc1850fc5c474ecc583
Remo Mueller <remosm@gmail.com>
2018-05-07 14:03:17 -0400

    Updated tests to ActionDispatch::IntegrationTest

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,169 +1,153 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
-# Tests creation of categories used to group designs on a project
-class CategoriesControllerTest < ActionController::TestCase
+# Tests creation of categories used to group designs on a project.
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    login(users(:valid))
+    @project_editor = users(:valid)
     @project = projects(:one)
     @category = categories(:one)
   end
 
-  test 'should get index' do
-    get :index, params: { project_id: @project }
+  def category_params
+    {
+      name: "New Category",
+      slug: "new-category",
+      description: "",
+      position: 3,
+      use_for_adverse_events: "0"
+    }
+  end
+
+  test "should get index" do
+    login(@project_editor)
+    get project_categories_url(@project)
     assert_response :success
     assert_not_nil assigns(:categories)
   end
 
-  test 'should not get index with invalid project' do
-    get :index, params: { project_id: -1 }
+  test "should not get index with invalid project" do
+    login(@project_editor)
+    get project_categories_url(-1)
     assert_nil assigns(:categories)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should get new' do
-    get :new, params: { project_id: @project }
+  test "should get new" do
+    login(@project_editor)
+    get new_project_category_url(@project)
     assert_response :success
   end
 
-  test 'should create category' do
-    assert_difference('Category.count') do
-      post :create, params: {
-        project_id: @project,
-        category: {
-          name: 'New Category',
-          slug: 'new-category',
-          description: @category.description,
-          position: @category.position,
-          use_for_adverse_events: @category.use_for_adverse_events
-        }
+  test "should create category" do
+    login(@project_editor)
+    assert_difference("Category.count") do
+      post project_categories_url(@project), params: {
+        category: category_params
       }
     end
-    assert_redirected_to project_category_path(assigns(:project), assigns(:category))
+    assert_redirected_to project_category_url(@project, Category.last)
   end
 
-  test 'should not create category with blank name' do
-    assert_difference('Category.count', 0) do
-      post :create, params: {
-        project_id: @project,
-        category: {
-          name: '',
-          slug: '',
-          description: @category.description,
-          position: @category.position,
-          use_for_adverse_events: @category.use_for_adverse_events
-        }
+  test "should not create category with blank name" do
+    login(@project_editor)
+    assert_difference("Category.count", 0) do
+      post project_categories_url(@project), params: {
+        category: category_params.merge(name: "", slug: "")
       }
     end
     assert_not_nil assigns(:category)
     assert_equal ["can't be blank"], assigns(:category).errors[:name]
-    assert_template 'new'
+    assert_template "new"
   end
 
-  test 'should not create category with invalid project' do
-    assert_difference('Category.count', 0) do
-      post :create, params: {
-        project_id: -1,
-        category: {
-          name: 'New Category',
-          slug: 'new-category',
-          description: @category.description,
-          position: @category.position,
-          use_for_adverse_events: @category.use_for_adverse_events
-        }
+  test "should not create category with invalid project" do
+    login(@project_editor)
+    assert_difference("Category.count", 0) do
+      post project_categories_url(-1), params: {
+        category: category_params
       }
     end
     assert_nil assigns(:category)
     assert_nil assigns(:project)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should show category' do
-    get :show, params: { project_id: @project, id: @category }
+  test "should show category" do
+    login(@project_editor)
+    get project_category_url(@project, @category)
     assert_not_nil assigns(:category)
     assert_response :success
   end
 
-  test 'should not show category with invalid project' do
-    get :show, params: { project_id: -1, id: @category }
+  test "should not show category with invalid project" do
+    login(@project_editor)
+    get project_category_url(-1, @category)
     assert_nil assigns(:category)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should get edit' do
-    get :edit, params: { project_id: @project, id: @category }
+  test "should get edit" do
+    login(@project_editor)
+    get edit_project_category_url(@project, @category)
     assert_not_nil assigns(:category)
     assert_response :success
   end
 
-  test 'should not get edit with invalid project' do
-    get :edit, params: { project_id: -1, id: @category }
+  test "should not get edit with invalid project" do
+    login(@project_editor)
+    get edit_project_category_url(-1, @category)
     assert_nil assigns(:category)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should update category' do
-    patch :update, params: {
-      project_id: @project, id: @category,
-      category: {
-        name: 'Updated Category',
-        slug: 'updated-category',
-        description: @category.description,
-        position: @category.position,
-        use_for_adverse_events: @category.use_for_adverse_events
-      }
+  test "should update category" do
+    login(@project_editor)
+    patch project_category_url(@project, @category), params: {
+      category: category_params.merge(name: "Updated Category", slug: "updated-category")
     }
-    assert_redirected_to project_category_path(assigns(:project), assigns(:category))
+    @category.reload
+    assert_redirected_to project_category_url(@project, @category)
   end
 
-  test 'should not update category with blank name' do
-    patch :update, params: {
-      project_id: @project, id: @category,
-      category: {
-        name: '',
-        slug: @category.slug,
-        description: @category.description,
-        position: @category.position,
-        use_for_adverse_events: @category.use_for_adverse_events
-      }
+  test "should not update category with blank name" do
+    login(@project_editor)
+    patch project_category_url(@project, @category), params: {
+      category: category_params.merge(name: "")
     }
     assert_not_nil assigns(:category)
     assert_equal ["can't be blank"], assigns(:category).errors[:name]
-    assert_template 'edit'
+    assert_template "edit"
   end
 
-  test 'should not update category with invalid project' do
-    patch :update, params: {
-      project_id: -1, id: @category,
-      category: {
-        name: 'Updated Category',
-        slug: 'updated-category',
-        description: @category.description,
-        position: @category.position,
-        use_for_adverse_events: @category.use_for_adverse_events
-      }
+  test "should not update category with invalid project" do
+    login(@project_editor)
+    patch project_category_url(-1, @category), params: {
+      category: category_params.merge(name: "Updated Category", slug: "updated-category")
     }
     assert_nil assigns(:category)
     assert_nil assigns(:project)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should destroy category' do
-    assert_difference('Category.current.count', -1) do
-      delete :destroy, params: { project_id: @project, id: @category }
+  test "should destroy category" do
+    login(@project_editor)
+    assert_difference("Category.current.count", -1) do
+      delete project_category_url(@project, @category)
     end
     assert_not_nil assigns(:category)
     assert_not_nil assigns(:project)
-    assert_redirected_to project_categories_path(assigns(:project))
+    assert_redirected_to project_categories_url(assigns(:project))
   end
 
-  test 'should not destroy category with invalid project' do
-    assert_difference('Category.current.count', 0) do
-      delete :destroy, params: { project_id: -1, id: @category }
+  test "should not destroy category with invalid project" do
+    login(@project_editor)
+    assert_difference("Category.current.count", 0) do
+      delete project_category_url(-1, @category)
     end
     assert_nil assigns(:category)
     assert_nil assigns(:project)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 end
