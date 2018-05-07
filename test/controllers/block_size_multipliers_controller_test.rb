0b9ffb1b0d317016953c540432b74af80f64ba3a
Remo Mueller <remosm@gmail.com>
2018-05-07 12:23:59 -0400

    Updated tests to ActionDispatch::IntegrationTest

--- test/controllers/block_size_multipliers_controller_test.rb
+++ test/controllers/block_size_multipliers_controller_test.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure that block size multipliers can be specified for randomization
 # schemes using permuted-block algorithm.
-class BlockSizeMultipliersControllerTest < ActionController::TestCase
+class BlockSizeMultipliersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    login(users(:valid))
+    @project_editor = users(:valid)
     @project = projects(:one)
     @published_scheme = randomization_schemes(:one)
     @published_block_size_multiplier = block_size_multipliers(:one)
@@ -14,157 +14,167 @@ class BlockSizeMultipliersControllerTest < ActionController::TestCase
     @block_size_multiplier = block_size_multipliers(:six)
   end
 
-  test 'should get index' do
-    get :index, params: { project_id: @project, randomization_scheme_id: @randomization_scheme }
+  def block_size_multiplier_params
+    { value: 5, allocation: 1 }
+  end
+
+  test "should get index" do
+    login(@project_editor)
+    get project_randomization_scheme_block_size_multipliers_url(@project, @randomization_scheme)
     assert_response :success
     assert_not_nil assigns(:block_size_multipliers)
   end
 
-  test 'should get new' do
-    get :new, params: { project_id: @project, randomization_scheme_id: @randomization_scheme }
+  test "should get new" do
+    login(@project_editor)
+    get new_project_randomization_scheme_block_size_multiplier_url(@project, @randomization_scheme)
     assert_response :success
   end
 
-  test 'should not get new for published randomization scheme' do
-    get :new, params: { project_id: @project, randomization_scheme_id: @published_scheme }
-    assert_redirected_to project_randomization_scheme_block_size_multipliers_path(
-      assigns(:project), assigns(:randomization_scheme)
+  test "should not get new for published randomization scheme" do
+    login(@project_editor)
+    get new_project_randomization_scheme_block_size_multiplier_url(@project, @published_scheme)
+    assert_redirected_to project_randomization_scheme_block_size_multipliers_url(
+      @project, @published_scheme
     )
   end
 
-  test 'should create block size multiplier' do
-    assert_difference('BlockSizeMultiplier.count') do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        block_size_multiplier: { value: 5, allocation: 1 }
+  test "should create block size multiplier" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.count") do
+      post project_randomization_scheme_block_size_multipliers_url(@project, @randomization_scheme), params: {
+        block_size_multiplier: block_size_multiplier_params
       }
     end
-
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, BlockSizeMultiplier.last
     )
   end
 
-  test 'should create block size multiplier with default values' do
-    assert_difference('BlockSizeMultiplier.count') do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        block_size_multiplier: { value: 2, allocation: '' }
+  test "should create block size multiplier with default values" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.count") do
+      post project_randomization_scheme_block_size_multipliers_url(@project, @randomization_scheme), params: {
+        block_size_multiplier: block_size_multiplier_params.merge(value: 2, allocation: "")
       }
     end
-
     assert_not_nil assigns(:block_size_multiplier)
     assert_equal 2, assigns(:block_size_multiplier).value
     assert_equal 0, assigns(:block_size_multiplier).allocation
-
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, BlockSizeMultiplier.last
     )
   end
 
-  test 'should not create block size multiplier for published randomization scheme' do
-    assert_difference('BlockSizeMultiplier.count', 0) do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @published_scheme,
-        block_size_multiplier: { value: 5, allocation: 1 }
+  test "should not create block size multiplier for published randomization scheme" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.count", 0) do
+      post project_randomization_scheme_block_size_multipliers_url(@project, @published_scheme), params: {
+        block_size_multiplier: block_size_multiplier_params
       }
     end
-
-    assert_redirected_to project_randomization_scheme_block_size_multipliers_path(
-      assigns(:project), assigns(:randomization_scheme)
+    assert_redirected_to project_randomization_scheme_block_size_multipliers_url(
+      @project, @published_scheme
     )
   end
 
-  test 'should not create block size multiplier with invalid value and allocation' do
-    assert_difference('BlockSizeMultiplier.count', 0) do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        block_size_multiplier: { value: 0, allocation: -1 }
+  test "should not create block size multiplier with invalid value and allocation" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.count", 0) do
+      post project_randomization_scheme_block_size_multipliers_url(@project, @randomization_scheme), params: {
+        block_size_multiplier: block_size_multiplier_params.merge(value: 0, allocation: -1)
       }
     end
-
     assert_not_nil assigns(:block_size_multiplier)
-    assert_equal ['must be greater than or equal to 1'], assigns(:block_size_multiplier).errors[:value]
-    assert_equal ['must be greater than or equal to 0'], assigns(:block_size_multiplier).errors[:allocation]
-    assert_template 'new'
+    assert_equal ["must be greater than or equal to 1"], assigns(:block_size_multiplier).errors[:value]
+    assert_equal ["must be greater than or equal to 0"], assigns(:block_size_multiplier).errors[:allocation]
+    assert_template "new"
   end
 
-  test 'should show block size multiplier' do
-    get :show, params: { project_id: @project, randomization_scheme_id: @randomization_scheme, id: @block_size_multiplier }
+  test "should show block size multiplier" do
+    login(@project_editor)
+    get project_randomization_scheme_block_size_multiplier_url(@project, @randomization_scheme, @block_size_multiplier)
     assert_response :success
   end
 
-  test 'should get edit' do
-    get :edit, params: { project_id: @project, randomization_scheme_id: @randomization_scheme, id: @block_size_multiplier }
+  test "should get edit" do
+    login(@project_editor)
+    get edit_project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, @block_size_multiplier
+    )
     assert_response :success
   end
 
-  test 'should not get edit for published randomization scheme' do
-    get :edit, params: { project_id: @project, randomization_scheme_id: @published_scheme, id: @published_block_size_multiplier }
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+  test "should not get edit for published randomization scheme" do
+    login(@project_editor)
+    get edit_project_randomization_scheme_block_size_multiplier_url(
+      @project, @published_scheme, @published_block_size_multiplier
+    )
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @published_scheme, @published_block_size_multiplier
     )
   end
 
-  test 'should update block size multiplier' do
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme,
-      id: @block_size_multiplier,
-      block_size_multiplier: { value: @block_size_multiplier.value, allocation: 3 }
+  test "should update block size multiplier" do
+    login(@project_editor)
+    patch project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, @block_size_multiplier
+    ), params: {
+      block_size_multiplier: { value: 1, allocation: 3 }
     }
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, @block_size_multiplier
     )
   end
 
-  test 'should not update block size multiplier for published randomization scheme' do
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @published_scheme,
-      id: @published_block_size_multiplier,
+  test "should not update block size multiplier for published randomization scheme" do
+    login(@project_editor)
+    patch project_randomization_scheme_block_size_multiplier_url(
+      @project, @published_scheme, @published_block_size_multiplier
+    ), params: {
       block_size_multiplier: { value: 2, allocation: 3 }
     }
     assert_equal 1, assigns(:block_size_multiplier).value
     assert_equal 2, assigns(:block_size_multiplier).allocation
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @published_scheme, @published_block_size_multiplier
     )
   end
 
-  test 'should not update block size multiplier with invalid value and allocation' do
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme,
-      id: @block_size_multiplier,
+  test "should not update block size multiplier with invalid value and allocation" do
+    login(@project_editor)
+    patch project_randomization_scheme_block_size_multiplier_url(
+      @project, @randomization_scheme, @block_size_multiplier
+    ), params: {
       block_size_multiplier: { value: 0, allocation: -1 }
     }
     assert_not_nil assigns(:block_size_multiplier)
-    assert_equal ['must be greater than or equal to 1'], assigns(:block_size_multiplier).errors[:value]
-    assert_equal ['must be greater than or equal to 0'], assigns(:block_size_multiplier).errors[:allocation]
-    assert_template 'edit'
+    assert_equal ["must be greater than or equal to 1"], assigns(:block_size_multiplier).errors[:value]
+    assert_equal ["must be greater than or equal to 0"], assigns(:block_size_multiplier).errors[:allocation]
+    assert_template "edit"
   end
 
-  test 'should destroy block size multiplier' do
-    assert_difference('BlockSizeMultiplier.current.count', -1) do
-      delete :destroy, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        id: @block_size_multiplier
-      }
+  test "should destroy block size multiplier" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.current.count", -1) do
+      delete project_randomization_scheme_block_size_multiplier_url(
+        @project, @randomization_scheme, @block_size_multiplier
+      )
     end
-
-    assert_redirected_to project_randomization_scheme_block_size_multipliers_path(
-      assigns(:project), assigns(:randomization_scheme)
+    assert_redirected_to project_randomization_scheme_block_size_multipliers_url(
+      @project, @randomization_scheme
     )
   end
 
-  test 'should not destroy block size multiplier for published randomization scheme' do
-    assert_difference('BlockSizeMultiplier.current.count', 0) do
-      delete :destroy, params: {
-        project_id: @project, randomization_scheme_id: @published_scheme,
-        id: @published_block_size_multiplier
-      }
+  test "should not destroy block size multiplier for published randomization scheme" do
+    login(@project_editor)
+    assert_difference("BlockSizeMultiplier.current.count", 0) do
+      delete project_randomization_scheme_block_size_multiplier_url(
+        @project, @published_scheme, @published_block_size_multiplier
+      )
     end
-
-    assert_redirected_to project_randomization_scheme_block_size_multiplier_path(
-      assigns(:project), assigns(:randomization_scheme), assigns(:block_size_multiplier)
+    assert_redirected_to project_randomization_scheme_block_size_multiplier_url(
+      @project, @published_scheme, @published_block_size_multiplier
     )
   end
 end
