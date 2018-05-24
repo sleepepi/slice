6d0e9b24cdb4995b98b9d67760d93222dc69c685
Remo Mueller <remosm@gmail.com>
2018-05-24 15:14:15 -0400

    Simplified with_site filter

--- app/models/concerns/siteable.rb
+++ app/models/concerns/siteable.rb
@@ -6,10 +6,7 @@ module Siteable
 
   included do
     def self.with_site(arg)
-      # TODO: The merge is preferred, however does not work with buildable.rb at
-      # the moment
-      # joins(:subject).merge(Subject.current.where(site_id: arg))
-      joins(:subject).where(subjects: { deleted: false, site_id: arg })
+      joins(:subject).merge(Subject.current.where(site_id: arg))
     end
   end
 end
