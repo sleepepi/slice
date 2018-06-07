0a8fc0b1cf3717d91f9a236aa3bce6c274d9bda5
Remo Mueller <remosm@gmail.com>
2018-06-07 13:18:13 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/validate_controller_test.rb
+++ test/controllers/validate_controller_test.rb
@@ -3,13 +3,13 @@
 require "test_helper"
 
 # Tests to assure variables on a project can be validated.
-class ValidateControllerTest < ActionController::TestCase
+class ValidateControllerTest < ActionDispatch::IntegrationTest
   test "should validate variable" do
-    post :variable, params: {
-      project_id: projects(:one), variable_id: variables(:date),
+    post validate_variable_url(format: "json"), params: {
+      project_id: projects(:one).to_param,
+      variable_id: variables(:date).id,
       value: { month: "1", day: "1", year: "2000" }
-    }, xhr: true, format: "json"
-
+    }
     json = JSON.parse(response.body)
     assert_equal "in_soft_range", json["status"]
     assert_equal "January 1, 2000", json["formatted_value"]
@@ -18,11 +18,11 @@ class ValidateControllerTest < ActionController::TestCase
   end
 
   test "should validate variable with blank fields" do
-    post :variable, params: {
-      project_id: projects(:one), variable_id: variables(:date),
+    post validate_variable_url(format: "json"), params: {
+      project_id: projects(:one).to_param,
+      variable_id: variables(:date).id,
       value: { month: "", day: "", year: "" }
-    }, xhr: true, format: "json"
-
+    }
     json = JSON.parse(response.body)
     assert_equal "blank", json["status"]
     assert_nil json["formatted_value"]
@@ -31,11 +31,11 @@ class ValidateControllerTest < ActionController::TestCase
   end
 
   test "should validate return out of range for variable" do
-    post :variable, params: {
-      project_id: projects(:one), variable_id: variables(:date),
+    post validate_variable_url(format: "json"), params: {
+      project_id: projects(:one).to_param,
+      variable_id: variables(:date).id,
       value: { month: "12", day: "31", year: "1989" }
-    }, xhr: true, format: "json"
-
+    }
     json = JSON.parse(response.body)
     assert_equal "out_of_range", json["status"]
     assert_equal "December 31, 1989", json["formatted_value"]
@@ -44,11 +44,11 @@ class ValidateControllerTest < ActionController::TestCase
   end
 
   test "should validate return invalid for variable" do
-    post :variable, params: {
-      project_id: projects(:one), variable_id: variables(:date),
+    post validate_variable_url(format: "json"), params: {
+      project_id: projects(:one).to_param,
+      variable_id: variables(:date).id,
       value: { month: "2", day: "31", year: "2000" }
-    }, xhr: true, format: "json"
-
+    }
     json = JSON.parse(response.body)
     assert_equal "invalid", json["status"]
     assert_nil json["formatted_value"]
@@ -57,11 +57,11 @@ class ValidateControllerTest < ActionController::TestCase
   end
 
   test "should validate return inside hard range for variable" do
-    post :variable, params: {
-      project_id: projects(:one), variable_id: variables(:date),
+    post validate_variable_url(format: "json"), params: {
+      project_id: projects(:one).to_param,
+      variable_id: variables(:date).id,
       value: { month: "6", day: "15", year: "1995" }
-    }, xhr: true, format: "json"
-
+    }
     json = JSON.parse(response.body)
     assert_equal "in_hard_range", json["status"]
     assert_equal "June 15, 1995", json["formatted_value"]
