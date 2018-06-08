aa763ea7e94fe4da0da9fc116e9d3b29d7d439b2
Remo Mueller <remosm@gmail.com>
2018-06-08 10:47:13 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/handoffs_controller_test.rb
+++ test/controllers/handoffs_controller_test.rb
@@ -1,116 +1,77 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Project editors should be able to launch handoffs for subjects with existing
 # subject events.
-class HandoffsControllerTest < ActionController::TestCase
+class HandoffsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @handoff = handoffs(:one)
     @project = projects(:one)
+    @project_editor = users(:project_one_editor)
+    @project_viewer = users(:project_one_viewer)
+    @site_editor = users(:site_one_editor)
   end
 
-  test 'should get new as project editor' do
-    login(users(:regular))
-    get :new, params: {
-      project_id: @project, id: subjects(:three),
-      subject_event_id: subject_events(:three)
-    }
-    assert_not_nil assigns(:project)
-    assert_not_nil assigns(:subject)
+  test "should get new as project editor" do
+    login(@project_editor)
+    get new_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
     assert_response :success
   end
 
-  test 'should get new as project editor for existing handoff' do
-    login(users(:regular))
-    get :new, params: {
-      project_id: @project, id: subjects(:two),
-      subject_event_id: subject_events(:two)
-    }
-    assert_not_nil assigns(:project)
-    assert_not_nil assigns(:subject)
+  test "should get new as project editor for existing handoff" do
+    login(@project_editor)
+    get new_handoff_project_subject_url(@project, subjects(:two), subject_events(:two))
     assert_response :success
   end
 
-  test 'should not get new as project viewer' do
-    login(users(:associated))
-    get :new, params: {
-      project_id: @project, id: subjects(:three),
-      subject_event_id: subject_events(:three)
-    }
-    assert_nil assigns(:project)
-    assert_nil assigns(:subject)
-    assert_redirected_to root_path
+  test "should not get new as project viewer" do
+    login(@project_viewer)
+    get new_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
+    assert_redirected_to root_url
   end
 
-  test 'should not get new as site editor from different site as subject' do
-    login(users(:site_one_editor))
-    get :new, params: {
-      project_id: @project, id: subjects(:three),
-      subject_event_id: subject_events(:three)
-    }
-    assert_not_nil assigns(:project)
-    assert_nil assigns(:subject)
-    assert_redirected_to project_subjects_path(assigns(:project))
+  test "should not get new as site editor from different site as subject" do
+    login(@site_editor)
+    get new_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
+    assert_redirected_to project_subjects_url(@project)
   end
 
-  test 'should launch new handoff as project editor' do
-    login(users(:regular))
-    assert_difference('Handoff.count') do
-      post :create, params: {
-        project_id: @project, id: subjects(:three),
-        subject_event_id: subject_events(:three)
-      }
+  test "should launch new handoff as project editor" do
+    login(@project_editor)
+    assert_difference("Handoff.count") do
+      post create_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
     end
-    assert_not_nil assigns(:project)
-    assert_not_nil assigns(:subject)
     assert_not_nil assigns(:handoff)
     assert_not_nil assigns(:handoff).token
-    assert_equal users(:regular), assigns(:handoff).user
-    assert_redirected_to handoff_start_path(assigns(:project), assigns(:handoff))
+    assert_equal @project_editor, assigns(:handoff).user
+    assert_redirected_to handoff_start_url(@project, assigns(:handoff))
   end
 
-  test 'should launch existing handoff as project editor' do
-    login(users(:regular))
-    assert_difference('Handoff.count', 0) do
-      post :create, params: {
-        project_id: @project, id: subjects(:two),
-        subject_event_id: subject_events(:two)
-      }
+  test "should launch existing handoff as project editor" do
+    login(@project_editor)
+    assert_difference("Handoff.count", 0) do
+      post create_handoff_project_subject_url(@project, subjects(:two), subject_events(:two))
     end
-    assert_not_nil assigns(:project)
-    assert_not_nil assigns(:subject)
     assert_not_nil assigns(:handoff)
     assert_not_nil assigns(:handoff).token
     assert_equal users(:admin), assigns(:handoff).user
-    assert_redirected_to handoff_start_path(assigns(:project), assigns(:handoff))
+    assert_redirected_to handoff_start_url(@project, assigns(:handoff))
   end
 
-  test 'should not launch new handoff as project viewer' do
-    login(users(:associated))
-    assert_difference('Handoff.count', 0) do
-      post :create, params: {
-        project_id: @project, id: subjects(:three),
-        subject_event_id: subject_events(:three)
-      }
+  test "should not launch new handoff as project viewer" do
+    login(@project_viewer)
+    assert_difference("Handoff.count", 0) do
+      post create_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
     end
-    assert_nil assigns(:project)
-    assert_nil assigns(:subject)
-    assert_nil assigns(:handoff)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should not launch new handoff as site editor from different site as subject' do
-    login(users(:site_one_editor))
-    assert_difference('Handoff.count', 0) do
-      post :create, params: {
-        project_id: @project, id: subjects(:three),
-        subject_event_id: subject_events(:three)
-      }
+  test "should not launch new handoff as site editor from different site as subject" do
+    login(@site_editor)
+    assert_difference("Handoff.count", 0) do
+      post create_handoff_project_subject_url(@project, subjects(:three), subject_events(:three))
     end
-    assert_not_nil assigns(:project)
-    assert_nil assigns(:subject)
-    assert_nil assigns(:handoff)
-    assert_redirected_to project_subjects_path(assigns(:project))
+    assert_redirected_to project_subjects_url(@project)
   end
 end
