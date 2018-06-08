ed402c303ee00ada9711cead88dc1a5cf1162bef
Remo Mueller <remosm@gmail.com>
2018-06-08 12:52:15 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/sheet_unlock_requests_controller_test.rb
+++ test/controllers/sheet_unlock_requests_controller_test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure that site editors can submit sheet unlock requests.
-class SheetUnlockRequestsControllerTest < ActionController::TestCase
+class SheetUnlockRequestsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @project = projects(:auto_lock)
     @locked_sheet = sheets(:auto_lock)
@@ -12,49 +12,53 @@ class SheetUnlockRequestsControllerTest < ActionController::TestCase
     @site_editor = users(:auto_lock_site_one_editor)
   end
 
-  test 'should create sheet unlock request' do
+  test "should create sheet unlock request" do
     login(@site_editor)
-    assert_difference('SheetUnlockRequest.count') do
-      post :create, params: {
-        project_id: @project, sheet_id: @locked_sheet,
-        sheet_unlock_request: { reason: 'Transcription Error' }
-      }, format: 'js'
-    end
-    assert_template 'create'
-    assert_response :success
-  end
-
-  test 'should not create sheet unlock request without reason' do
-    login(@site_editor)
-    assert_difference('SheetUnlockRequest.count', 0) do
-      post :create, params: {
-        project_id: @project, sheet_id: @locked_sheet,
-        sheet_unlock_request: { reason: '' }
-      }, format: 'js'
-    end
-    assert_template 'new'
-    assert_response :success
-  end
-
-  test 'should destroy sheet unlock request as site editor' do
-    login(@site_editor)
-    assert_difference('SheetUnlockRequest.current.count', -1) do
-      delete :destroy, params: {
-        project_id: @project, sheet_id: @locked_sheet,
-        id: @sheet_unlock_request
+    assert_difference("SheetUnlockRequest.count") do
+      post project_sheet_sheet_unlock_requests_url(
+        @project, @locked_sheet, format: "js"
+      ), params: {
+        sheet_unlock_request: { reason: "Transcription Error" }
       }
+    end
+    assert_template "create"
+    assert_response :success
+  end
+
+  test "should not create sheet unlock request without reason" do
+    login(@site_editor)
+    assert_difference("SheetUnlockRequest.count", 0) do
+      post project_sheet_sheet_unlock_requests_url(
+        @project, @locked_sheet, format: "js"
+      ), params: {
+        sheet_unlock_request: { reason: "" }
+      }
+    end
+    assert_template "new"
+    assert_response :success
+  end
+
+  test "should destroy sheet unlock request as site editor" do
+    login(@site_editor)
+    assert_difference("SheetUnlockRequest.current.count", -1) do
+      delete project_sheet_sheet_unlock_request_url(
+        @project,
+        @locked_sheet,
+        @sheet_unlock_request
+      )
     end
     assert_redirected_to [@project, @locked_sheet]
   end
 
-  test 'should destroy sheet unlock request as project editor' do
+  test "should destroy sheet unlock request as project editor" do
     login(@project_editor)
-    assert_difference('Notification.count', -1) do
-      assert_difference('SheetUnlockRequest.current.count', -1) do
-        delete :destroy, params: {
-          project_id: @project, sheet_id: @locked_sheet,
-          id: @sheet_unlock_request
-        }
+    assert_difference("Notification.count", -1) do
+      assert_difference("SheetUnlockRequest.current.count", -1) do
+        delete project_sheet_sheet_unlock_request_url(
+          @project,
+          @locked_sheet,
+          @sheet_unlock_request
+        )
       end
     end
     assert_redirected_to [@project, @locked_sheet]
