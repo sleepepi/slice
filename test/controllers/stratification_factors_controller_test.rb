972357df990cf71677998c119af2015bfebfaf7c
Remo Mueller <remosm@gmail.com>
2018-06-08 13:23:45 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/stratification_factors_controller_test.rb
+++ test/controllers/stratification_factors_controller_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Allows project editors to modify stratification factors for randomization
 # schemes.
-class StratificationFactorsControllerTest < ActionController::TestCase
+class StratificationFactorsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @project_editor = users(:project_one_editor)
     @project = projects(:one)
@@ -22,192 +22,184 @@ class StratificationFactorsControllerTest < ActionController::TestCase
     }
   end
 
-  test 'should get index' do
+  test "should get index" do
     login(@project_editor)
-    get :index, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme
-    }
+    get project_randomization_scheme_stratification_factors_url(
+      @project, @randomization_scheme
+    )
     assert_response :success
     assert_not_nil assigns(:stratification_factors)
   end
 
-  test 'should get new' do
+  test "should get new" do
     login(@project_editor)
-    get :new, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme
-    }
+    get new_project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme
+    )
     assert_response :success
   end
 
-  test 'should not get new for published randomization scheme' do
+  test "should not get new for published randomization scheme" do
     login(@project_editor)
-    get :new, params: {
-      project_id: @project, randomization_scheme_id: @published_scheme
-    }
+    get new_project_randomization_scheme_stratification_factor_url(
+      @project, @published_scheme
+    )
     assert_redirected_to(
-      project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
+      project_randomization_scheme_stratification_factors_url(assigns(:project), assigns(:randomization_scheme))
     )
   end
 
-  test 'should create stratification factor' do
+  test "should create stratification factor" do
     login(@project_editor)
-    assert_difference('StratificationFactor.count') do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        stratification_factor: sf_params.merge(name: 'New Stratification Factor')
+    assert_difference("StratificationFactor.count") do
+      post project_randomization_scheme_stratification_factors_url(
+        @project, @randomization_scheme
+      ), params: {
+        stratification_factor: sf_params.merge(name: "New Stratification Factor")
       }
     end
-
     assert_redirected_to(
-      project_randomization_scheme_stratification_factor_path(
+      project_randomization_scheme_stratification_factor_url(
+        @project, @randomization_scheme, StratificationFactor.last
+      )
+    )
+  end
+
+  test "should not create stratification factor with blank name" do
+    login(@project_editor)
+    assert_difference("StratificationFactor.count", 0) do
+      post project_randomization_scheme_stratification_factors_url(
+        @project, @randomization_scheme
+      ), params: {
+        stratification_factor: sf_params.merge(name: "")
+      }
+    end
+    assert_template "new"
+    assert_response :success
+  end
+
+  test "should not create stratification factor for published randomization scheme" do
+    login(@project_editor)
+    assert_difference("StratificationFactor.count", 0) do
+      post project_randomization_scheme_stratification_factors_url(
+        @project, @published_scheme
+      ), params: {
+        stratification_factor: sf_params.merge(name: "New Stratification Factor")
+      }
+    end
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factors_url(assigns(:project), assigns(:randomization_scheme))
+    )
+  end
+
+  test "should show stratification factor" do
+    login(@project_editor)
+    get project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme, @stratification_factor
+    )
+    assert_response :success
+  end
+
+  test "should not show stratification factor with invalid randomization scheme" do
+    login(@project_editor)
+    get project_randomization_scheme_stratification_factor_url(
+      @project, -1, @stratification_factor
+    )
+    assert_redirected_to project_randomization_schemes_url(@project)
+  end
+
+  test "should not show stratification factor with invalid stratification factor" do
+    login(@project_editor)
+    get project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme, -1
+    )
+    assert_redirected_to project_randomization_scheme_stratification_factors_url(@project, @randomization_scheme)
+  end
+
+  test "should get edit" do
+    login(@project_editor)
+    get edit_project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme, @stratification_factor
+    )
+    assert_response :success
+  end
+
+  test "should not get edit for published randomization scheme" do
+    login(@project_editor)
+    get edit_project_randomization_scheme_stratification_factor_url(
+      @project, @published_scheme, @published_stratification_factor
+    )
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_url(
+        @project, @published_scheme, @published_stratification_factor
+      )
+    )
+  end
+
+  test "should update stratification factor" do
+    login(@project_editor)
+    patch project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme, @stratification_factor
+    ), params: {
+      stratification_factor: sf_params.merge(name: "Updated Stratification Factor")
+    }
+    assert_redirected_to(
+      project_randomization_scheme_stratification_factor_url(
         assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
       )
     )
   end
 
-  test 'should not create stratification factor with blank name' do
+  test "should not update stratification factor with blank name" do
     login(@project_editor)
-    assert_difference('StratificationFactor.count', 0) do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        stratification_factor: sf_params.merge(name: '')
-      }
-    end
-    assert_template 'new'
+    patch project_randomization_scheme_stratification_factor_url(
+      @project, @randomization_scheme, @stratification_factor
+    ), params: {
+      stratification_factor: sf_params.merge(name: "")
+    }
+    assert_template "edit"
     assert_response :success
   end
 
-  test 'should not create stratification factor for published randomization scheme' do
+  test "should not update stratification factor for published randomization scheme" do
     login(@project_editor)
-    assert_difference('StratificationFactor.count', 0) do
-      post :create, params: {
-        project_id: @project, randomization_scheme_id: @published_scheme,
-        stratification_factor: sf_params.merge(name: 'New Stratification Factor')
-      }
-    end
+    patch project_randomization_scheme_stratification_factor_url(
+      @project, @published_scheme, @published_stratification_factor
+    ), params: {
+      stratification_factor: sf_params.merge(name: "Updated Stratification Factor")
+    }
+    assert_equal "Gender", assigns(:stratification_factor).name
     assert_redirected_to(
-      project_randomization_scheme_stratification_factors_path(assigns(:project), assigns(:randomization_scheme))
-    )
-  end
-
-  test 'should show stratification factor' do
-    login(@project_editor)
-    get :show, params: {
-      project_id: @project,
-      randomization_scheme_id: @randomization_scheme, id: @stratification_factor
-    }
-    assert_response :success
-  end
-
-  test 'should not show stratification factor with invalid randomization scheme' do
-    login(@project_editor)
-    get :show, params: {
-      project_id: @project,
-      randomization_scheme_id: -1, id: @stratification_factor
-    }
-    assert_redirected_to project_randomization_schemes_path(@project)
-  end
-
-  test 'should not show stratification factor with invalid stratification factor' do
-    login(@project_editor)
-    get :show, params: {
-      project_id: @project,
-      randomization_scheme_id: @randomization_scheme, id: -1
-    }
-    assert_redirected_to project_randomization_scheme_stratification_factors_path(@project, @randomization_scheme)
-  end
-
-  test 'should get edit' do
-    login(@project_editor)
-    get :edit, params: {
-      project_id: @project,
-      randomization_scheme_id: @randomization_scheme, id: @stratification_factor
-    }
-    assert_response :success
-  end
-
-  test 'should not get edit for published randomization scheme' do
-    login(@project_editor)
-    get :edit, params: {
-      project_id: @project,
-      randomization_scheme_id: @published_scheme,
-      id: @published_stratification_factor
-    }
-    assert_redirected_to(
-      project_randomization_scheme_stratification_factor_path(
+      project_randomization_scheme_stratification_factor_url(
         assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
       )
     )
   end
 
-  test 'should update stratification factor' do
+  test "should destroy stratification factor" do
     login(@project_editor)
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme,
-      id: @stratification_factor,
-      stratification_factor: sf_params.merge(name: 'Updated Stratification Factor')
-    }
-    assert_redirected_to(
-      project_randomization_scheme_stratification_factor_path(
-        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+    assert_difference("StratificationFactor.current.count", -1) do
+      delete project_randomization_scheme_stratification_factor_url(
+        @project, @randomization_scheme, @stratification_factor
       )
-    )
-  end
-
-  test 'should update stratification factor with blank name' do
-    login(@project_editor)
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @randomization_scheme,
-      id: @stratification_factor,
-      stratification_factor: sf_params.merge(name: '')
-    }
-    assert_template 'edit'
-    assert_response :success
-  end
-
-  test 'should not update stratification factor for published randomization scheme' do
-    login(@project_editor)
-    patch :update, params: {
-      project_id: @project, randomization_scheme_id: @published_scheme,
-      id: @published_stratification_factor,
-      stratification_factor: sf_params.merge(name: 'Updated Stratification Factor')
-    }
-    assert_equal 'Gender', assigns(:stratification_factor).name
-    assert_redirected_to(
-      project_randomization_scheme_stratification_factor_path(
-        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
-      )
-    )
-  end
-
-  test 'should destroy stratification factor' do
-    login(@project_editor)
-    assert_difference('StratificationFactor.current.count', -1) do
-      delete :destroy, params: {
-        project_id: @project, randomization_scheme_id: @randomization_scheme,
-        id: @stratification_factor
-      }
     end
-
     assert_redirected_to(
-      project_randomization_scheme_stratification_factors_path(
-        assigns(:project), assigns(:randomization_scheme)
+      project_randomization_scheme_stratification_factors_url(
+        @project, @randomization_scheme
       )
     )
   end
 
-  test 'should not destroy stratification factor for published randomization scheme' do
+  test "should not destroy stratification factor for published randomization scheme" do
     login(@project_editor)
-    assert_difference('StratificationFactor.current.count', 0) do
-      delete :destroy, params: {
-        project_id: @project, randomization_scheme_id: @published_scheme,
-        id: @published_stratification_factor
-      }
+    assert_difference("StratificationFactor.current.count", 0) do
+      delete project_randomization_scheme_stratification_factor_url(
+        @project, @published_scheme, @published_stratification_factor
+      )
     end
-
     assert_redirected_to(
-      project_randomization_scheme_stratification_factor_path(
-        assigns(:project), assigns(:randomization_scheme), assigns(:stratification_factor)
+      project_randomization_scheme_stratification_factor_url(
+        @project, @published_scheme, @published_stratification_factor
       )
     )
   end
