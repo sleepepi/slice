5e7c2c743c45ea23c911a820bb8e5529d1174521
Remo Mueller <remosm@gmail.com>
2018-06-08 12:58:59 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to make sure project editors can edit sites, and that project and site
 # members can view sites.
-class SitesControllerTest < ActionController::TestCase
+class SitesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @project = projects(:one)
     @site = sites(:one)
@@ -21,227 +21,215 @@ class SitesControllerTest < ActionController::TestCase
     }
   end
 
-  test 'should get setup' do
+  test "should get setup" do
     login(@project_editor)
-    get :setup, params: { project_id: @project }
+    get setup_project_sites_url(@project)
     assert_response :success
   end
 
-  test 'should get add site row' do
+  test "should get add site row" do
     login(@project_editor)
-    post :add_site_row, params: { project_id: @project }, format: 'js'
-    assert_template 'add_site_row'
+    post add_site_row_project_sites_url(@project, format: "js")
+    assert_template "add_site_row"
     assert_response :success
   end
 
-  test 'should get create site and keep default site short name' do
+  test "should get create site and keep default site short name" do
     login(users(:regular))
-    assert_difference('Site.count', 0) do
-      post :create_sites, params: {
-        project_id: projects(:default),
+    assert_difference("Site.count", 0) do
+      post create_sites_project_sites_url(projects(:default), format: "js"), params: {
         sites: [
-          { id: sites(:default_site).id, name: 'Default Site' }
+          { id: sites(:default_site).id, name: "Default Site" }
         ]
-      }, format: 'js'
+      }
     end
     sites(:default_site).reload
-    assert_equal 'Default Site', sites(:default_site).short_name
-    assert_redirected_to invite_editor_project_path(projects(:default))
+    assert_equal "Default Site", sites(:default_site).short_name
+    assert_redirected_to invite_editor_project_url(projects(:default))
   end
 
-  test 'should get create site and remove default site short name' do
+  test "should get create site and remove default site short name" do
     login(users(:regular))
-    assert_difference('Site.count', 0) do
-      post :create_sites, params: {
-        project_id: projects(:default),
+    assert_difference("Site.count", 0) do
+      post create_sites_project_sites_url(projects(:default), format: "js"), params: {
         sites: [
-          { id: sites(:default_site).id, name: 'New Name' }
+          { id: sites(:default_site).id, name: "New Name" }
         ]
-      }, format: 'js'
+      }
     end
     sites(:default_site).reload
-    assert_equal 'NN', sites(:default_site).short_name
-    assert_redirected_to invite_editor_project_path(projects(:default))
+    assert_equal "NN", sites(:default_site).short_name
+    assert_redirected_to invite_editor_project_url(projects(:default))
   end
 
-  test 'should get create sites' do
+  test "should get create sites" do
     login(@project_editor)
-    assert_difference('Site.count') do
-      post :create_sites, params: {
-        project_id: @project,
+    assert_difference("Site.count") do
+      post create_sites_project_sites_url(@project, format: "js"), params: {
         sites: [
           { id: sites(:one).id, name: sites(:one).name },
-          { id: '', name: 'New Site' }
+          { id: "", name: "New Site" }
         ]
-      }, format: 'js'
+      }
     end
-    assert_redirected_to invite_editor_project_path(@project)
+    assert_redirected_to invite_editor_project_url(@project)
   end
 
-  test 'should get index' do
+  test "should get index" do
     login(@project_editor)
-    get :index, params: { project_id: @project }
+    get project_sites_url(@project)
     assert_response :success
   end
 
-  test 'should get paginated index' do
+  test "should not get index with invalid project" do
     login(@project_editor)
-    get :index, params: { project_id: @project }, format: 'js'
-    assert_template 'index'
+    get project_sites_url(-1)
+    assert_redirected_to root_url
+  end
+
+  test "should get new" do
+    login(@project_editor)
+    get new_project_site_url(@project)
     assert_response :success
   end
 
-  test 'should not get index with invalid project' do
+  test "should not get new site with invalid project" do
     login(@project_editor)
-    get :index, params: { project_id: -1 }
-    assert_redirected_to root_path
+    get new_project_site_url(-1)
+    assert_redirected_to root_url
   end
 
-  test 'should get new' do
+  test "should create site" do
     login(@project_editor)
-    get :new, params: { project_id: @project }
-    assert_response :success
-  end
-
-  test 'should not get new site with invalid project' do
-    login(@project_editor)
-    get :new, params: { project_id: -1 }
-    assert_redirected_to root_path
-  end
-
-  test 'should create site' do
-    login(@project_editor)
-    assert_difference('Site.count') do
-      post :create, params: {
-        project_id: @project, site: site_params.merge(name: 'Site New')
+    assert_difference("Site.count") do
+      post project_sites_url(@project), params: {
+        site: site_params.merge(name: "Site New")
       }
     end
     assert_redirected_to [@project, Site.last]
   end
 
-  test 'should not create site with blank name' do
+  test "should not create site with blank name" do
     login(@project_editor)
-    assert_difference('Site.count', 0) do
-      post :create, params: {
-        project_id: @project, site: site_params.merge(name: '')
+    assert_difference("Site.count", 0) do
+      post project_sites_url(@project), params: {
+        site: site_params.merge(name: "")
       }
     end
     assert_not_nil assigns(:site)
     assert_equal ["can't be blank"], assigns(:site).errors[:name]
-    assert_template 'new'
+    assert_template "new"
     assert_response :success
   end
 
-  test 'should not create site for invalid project' do
+  test "should not create site for invalid project" do
     login(@project_editor)
-    assert_difference('Site.count', 0) do
-      post :create, params: {
-        project_id: -1, site: site_params.merge(name: 'Site New')
+    assert_difference("Site.count", 0) do
+      post project_sites_url(-1), params: {
+        site: site_params.merge(name: "Site New")
       }
     end
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should not create site for site viewer' do
+  test "should not create site for site viewer" do
     login(@site_viewer)
-    assert_difference('Site.count', 0) do
-      post :create, params: {
-        project_id: @project, site: site_params.merge(name: 'Site New')
+    assert_difference("Site.count", 0) do
+      post project_sites_url(@project), params: {
+        site: site_params.merge(name: "Site New")
       }
     end
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should show site' do
+  test "should show site" do
     login(@project_editor)
-    get :show, params: { project_id: @project, id: @site }
+    get project_site_url(@project, @site)
     assert_response :success
   end
 
-  test 'should show site for site viewer' do
+  test "should show site for site viewer" do
     login(@site_viewer)
-    get :show, params: { project_id: @project, id: @site }
+    get project_site_url(@project, @site)
     assert_response :success
   end
 
-  test 'should not show invalid site' do
+  test "should not show invalid site" do
     login(@project_editor)
-    get :show, params: { project_id: @project, id: -1 }
-    assert_redirected_to project_sites_path
+    get project_site_url(@project, -1)
+    assert_redirected_to project_sites_url(@project)
   end
 
-  test 'should not show site with invalid project' do
+  test "should not show site with invalid project" do
     login(@project_editor)
-    get :show, params: { project_id: -1, id: @site }
-    assert_redirected_to root_path
+    get project_site_url(-1, @site)
+    assert_redirected_to root_url
   end
 
-  test 'should get edit' do
+  test "should get edit" do
     login(@project_editor)
-    get :edit, params: { project_id: @project, id: @site }
+    get edit_project_site_url(@project, @site)
     assert_response :success
   end
 
-  test 'should not get edit for invalid site' do
+  test "should not get edit for invalid site" do
     login(@project_editor)
-    get :edit, params: { project_id: @project, id: -1 }
-    assert_redirected_to project_sites_path(@project)
+    get edit_project_site_url(@project, -1)
+    assert_redirected_to project_sites_url(@project)
   end
 
-  test 'should not get edit with invalid project' do
+  test "should not get edit with invalid project" do
     login(@project_editor)
-    get :edit, params: { project_id: -1, id: @site }
+    get edit_project_site_url(-1, @site)
     assert_nil assigns(:project)
     assert_nil assigns(:site)
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should update site' do
+  test "should update site" do
     login(@project_editor)
-    patch :update, params: {
-      project_id: @project, id: @site, site: site_params
-    }
-    assert_redirected_to project_site_path(@project, @site)
+    patch project_site_url(@project, @site), params: { site: site_params }
+    assert_redirected_to project_site_url(@project, @site)
   end
 
-  test 'should not update site with blank name' do
+  test "should not update site with blank name" do
     login(@project_editor)
-    patch :update, params: {
-      project_id: @project, id: @site, site: site_params.merge(name: '')
+    patch project_site_url(@project, @site), params: {
+      site: site_params.merge(name: "")
     }
     assert_not_nil assigns(:site)
     assert_equal ["can't be blank"], assigns(:site).errors[:name]
-    assert_template 'edit'
+    assert_template "edit"
     assert_response :success
   end
 
-  test 'should not update invalid site' do
+  test "should not update invalid site" do
     login(@project_editor)
-    patch :update, params: { project_id: @project, id: -1, site: site_params }
-    assert_redirected_to project_sites_path(@project)
+    patch project_site_url(@project, -1), params: { site: site_params }
+    assert_redirected_to project_sites_url(@project)
   end
 
-  test 'should not update with invalid project' do
+  test "should not update with invalid project" do
     login(@project_editor)
-    patch :update, params: { project_id: -1, id: @site, site: site_params }
-    assert_redirected_to root_path
+    patch project_site_url(-1, @site), params: { site: site_params }
+    assert_redirected_to root_url
   end
 
-  test 'should destroy site' do
+  test "should destroy site" do
     login(@project_editor)
-    assert_difference('Subject.current.count', -3) do
-      assert_difference('Site.current.count', -1) do
-        delete :destroy, params: { project_id: @project, id: @site }
+    assert_difference("Subject.current.count", -3) do
+      assert_difference("Site.current.count", -1) do
+        delete project_site_url(@project, @site)
       end
     end
-    assert_redirected_to project_sites_path(@project)
+    assert_redirected_to project_sites_url(@project)
   end
 
-  test 'should not destroy site with invalid project' do
+  test "should not destroy site with invalid project" do
     login(@project_editor)
-    assert_difference('Site.current.count', 0) do
-      delete :destroy, params: { project_id: -1, id: @site }
+    assert_difference("Site.current.count", 0) do
+      delete project_site_url(-1, @site)
     end
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 end
