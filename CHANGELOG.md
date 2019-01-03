f1d0a38d1a9e83b74c867f664a75c4e73dde1d6e
Remo Mueller <remosm@gmail.com>
2019-01-03 13:08:23 -0500

    Update to carrierwave 1.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
   - Update to ruby 2.6.0
   - Update to rails 5.2.2
   - Update to bootstrap 4.2.1
+  - Update to carrierwave 1.3.1
   - Update to font-awesome-sass 5.6.1
 
 ## 68.0.1 (November 14, 2018)
--- gems.rb
+++ gems.rb
@@ -10,7 +10,7 @@ gem "pg",                   "1.1.3"
 # Gems used by project
 gem "autoprefixer-rails"
 gem "bootstrap",            "~> 4.2.1"
-gem "carrierwave",          "~> 1.2.3"
+gem "carrierwave",          "~> 1.3.1"
 gem "chunky_png",           "~> 1.3.10"
 gem "devise",               "~> 4.5.0"
 gem "differ",               "~> 0.1.2"
