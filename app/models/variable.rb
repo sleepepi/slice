d943d4eff0b3ac4feb245dde285e2d24a6e74804
Remo Mueller <remosm@gmail.com>
2019-01-04 14:59:37 -0500

    Changing a variable domain should only affect responses associated to the variable itself, and not responses for other variables that may share the same domain

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -450,8 +450,8 @@ class Variable < ApplicationRecord
     (old_domain_id, new_domain_id) = saved_changes[:domain_id]
     old_domain = project.domains.find_by(id: old_domain_id)
     new_domain = project.domains.find_by(id: new_domain_id)
-    old_domain.remove_domain_values! if old_domain
-    new_domain.add_domain_values! if new_domain
+    old_domain.remove_domain_values!(self) if old_domain
+    new_domain.add_domain_values!(self) if new_domain
   end
 
   def save_translation!(variable_params)
