114222216ab6c7cd6653a584a90be2d5a5b20631
Remo Mueller <remosm@gmail.com>
2019-03-11 12:19:29 -0400

    Project medications also filters out medications for deleted subjects, addresses #697.

--- app/models/concerns/medications.rb
+++ app/models/concerns/medications.rb
@@ -6,7 +6,7 @@ module Medications
   included do
     # Relationships
     has_many :medication_variables, -> { current.order("position nulls last") }
-    has_many :medications, -> { current }
+    has_many :medications, -> { current.joins(:subject).merge(Subject.current) }
     has_many :medication_values
   end
 end
