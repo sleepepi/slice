d6d2759c3778ca8e7470ead44884ea23ff19590c
Remo Mueller <remosm@gmail.com>
2019-04-13 13:15:32 -0400

    Note to change this for Amazon S3 file formatting

--- app/models/formatters/file_formatter.rb
+++ app/models/formatters/file_formatter.rb
@@ -5,6 +5,7 @@ module Formatters
   class FileFormatter < DefaultFormatter
     def name_response(response_file)
       response_file.to_s.split("/").last if response_file && response_file.size > 0
+      # self[:response_file] # TODO: Change for Amazon S3...
     end
 
     # def raw_response(response_file)
