59866403ded9fe49a227ae43f7345393763af440
Remo Mueller <remosm@gmail.com>
2019-06-02 11:34:06 -0400

    Version bump to 77.0.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,10 +2,10 @@
 
 module Slice
   module VERSION #:nodoc:
-    MAJOR = 76
+    MAJOR = 77
     MINOR = 0
     TINY = 0
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
